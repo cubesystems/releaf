ea9dfd540d98a95671a94cfe0f4365b459237eb5
Aldis Berjoza <aldis@cubesystems.lv>
2012-12-17 16:12:23 +0200

    Make cancan work for admins :)

--- app/controllers/leaf_rails/application_controller.rb
+++ app/controllers/leaf_rails/application_controller.rb
@@ -1,5 +1,10 @@
 module LeafRails
   class ApplicationController < ActionController::Base
     before_filter :authenticate_admin!
+    check_authorization :unless => :devise_controller?
+
+    def current_ability
+      @current_ability ||= AdminAbility.new(current_admin)
+    end
   end
 end
