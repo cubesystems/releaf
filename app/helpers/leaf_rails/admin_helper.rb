ef3c29d8a3781e05148b088a88e958356e64626e
Aldis Berjoza <aldis@cubesystems.lv>
2012-12-18 17:00:43 +0200

    Fix helper

--- app/helpers/leaf_rails/admin_helper.rb
+++ app/helpers/leaf_rails/admin_helper.rb
@@ -3,12 +3,12 @@ module LeafRails
 
     def menu
       menu = {
-        :items => ["leaf_rails/content", "admins", "leaf_rails/aliases"],
+        :items => ["leaf_rails/content", "admin/admins", "leaf_rails/aliases"],
         :active => "admins",
       }
 
-      if menu[:items].include?( params[:controller].split('/').pop() )
-        menu[:active] = params[:controller].split('/').pop()
+      if menu[:items].include?( params[:controller] )
+        menu[:active] = params[:controller]
       end
 
       return menu
