6ce0c3aa7ccc5017e79b47c61a3fd0d2fc0a8945
Aldis Berjoza <aldis@cubesystems.lv>
2012-12-19 10:06:36 +0200

    Cleanup controller

--- app/controllers/leaf_rails/sessions_controller.rb
+++ app/controllers/leaf_rails/sessions_controller.rb
@@ -1,11 +1,5 @@
 module LeafRails
   class SessionsController < Devise::SessionsController
     layout "leaf_rails/admin"
-
-    def new
-      # redirect_to "/status/"
-      super
-      # render :layout => 'admin'
-    end
   end
 end
