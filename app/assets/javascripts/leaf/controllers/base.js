40826b18486a0bd54028eb2b72b88001c69c63c7
Aldis Berjoza <aldis@cubesystems.lv>
2013-01-08 09:18:10 +0200

    Improve date/time pickers

--- app/assets/javascripts/leaf/controllers/base.js
+++ app/assets/javascripts/leaf/controllers/base.js
@@ -128,14 +128,33 @@ jQuery(function()
             controlType: 'select',
             showHour: true,
             showMinute: true,
-            showSecond: true,
-            showTimezone: true,
-            pickerTimeFormat: 'HH:mm:ss',
-            dateFormat:       'yy-mm-dd',
+            // showSecond: true,
+            // showTimezone: true,
         }
-        forms.find('.date_picker').datepicker(options);
-        forms.find('.datetime_picker').datetimepicker(options);
-        forms.find('.time_picker').timepicker(options);
+
+        forms.find('.date_picker').each(function() {
+            var picker = jQuery(this);
+            var opt = options;
+            opt['dateFormat'] = picker.attr('data-date-format') || 'yy-mm-dd';
+            picker.datepicker(opt);
+        });
+
+        forms.find('.datetime_picker').each(function() {
+            var picker = jQuery(this);
+            var opt = options;
+            opt['dateFormat'] = picker.attr('data-date-format') || 'yy-mm-dd';
+            opt['pickerTimeFormat'] = picker.attr('data-time-format') || 'HH:mm'
+            picker.datetimepicker(opt);
+        });
+
+        /*
+         * forms.find('.time_picker').each(function() {
+         *     var picker = jQuery(this);
+         *     var opt = options;
+         *     opt['pickerTimeFormat'] = picker.attr('data-time-format') || 'HH:mm'
+         *     picker.timepicker(options);
+         * });
+         */
     });
 
     jQuery('form').trigger('initcalendars');
