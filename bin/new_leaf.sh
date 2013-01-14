913044622daa115512cda3ed09a9a6971844ecfd
Aldis Berjoza <aldis@cubesystems.lv>
2013-01-14 11:59:25 +0200

    Update

--- bin/new_leaf.sh
+++ bin/new_leaf.sh
@@ -175,11 +175,11 @@ cat << EOF > "$app_name/app/views/layouts/application.html.erb"
     = javascript_include_tag "application"
 EOF
 
-echo "rvm 1.9.3@$app_name" > "$app_name/.rvmrc"
+cd $app_name
+echo "rvm 1.9.3@$app_name" > ".rvmrc"
 rvm gemset create $app_name
 rvm gemset use $app_name
 
-cd $app_name
 bundle install
 
 rake db:drop db:create
