571c481acb304afd47e1a48c9770c50b47f258a0
miks <miks@cubesystems.lv>
2013-01-24 00:08:41 +0200

    Remove legacy methods

--- app/models/leaf/node.rb
+++ app/models/leaf/node.rb
@@ -35,6 +35,9 @@ module Leaf
       end
     end
 
+    ##
+    # Return node public url
+
     def url
       if parent_id
         url = parent.url + "/" + slug.to_s
@@ -45,88 +48,12 @@ module Leaf
       url
     end
 
+    ##
+    # Return node content object public controller
+
     def controller
-      controller_class = nil
-
-      if content_object
-        controller_class = "#{content_type}_controller".classify.constantize
-      end
-
-      controller_class
-    end
-
-    def is_controller_node
-      if  (content_type =~ /Controller$/i) != nil && content_type.constantize < LeafController
-        return true
-      else
-        return false
-      end
-    end
-
-    def self.maintain_base_controllers
-      locales = Settings.i18n_locales || ['en']
-      tree = {}
-
-      # 1) build up controller tree
-      Rails.application.routes.routes.routes.map do|r|
-        # skip /admin controllers
-        if (r.path.spec.to_s =~ /^\/admin/) == nil && !r.defaults[:controller].to_s.empty?
-          class_name = "#{r.defaults[:controller]}_controller".classify.constantize
-          if class_name < LeafController
-            path = r.path.spec.to_s.gsub("(.:format)", "")
-            path = path.split("/").reject(&:empty?)
-
-            item = {:controller => class_name, :action => r.defaults[:action]}
-
-            if path.last != ":id"
-              if path[0] == ":locale"
-                locales.each do |locale|
-                  path[0] = locale
-                  tree[path.join("/")] = item
-                end
-              else
-                tree[path.join("/")] = item
-              end
-            end
-          end
-        end
-      end
-
-      # 2) maintain tree against node content
-      tree = Hash[tree.sort]
-      tree.each do | url, item |
-        parent_path = url.split("/")[0...-1].join("/")
-        create = false
-        n = nil
-
-        slug = url.split("/").last
-        content_string = slug + ":" + item[:action].to_s
-
-        if parent_path.empty?
-          parent_id = nil
-          n = self::get_object_from_path url, :strict => true
-          if !n
-            create = true
-          end
-        else
-          parent_item = tree[parent_path]
-          if parent_item && parent_item[:node]
-            parent_id = parent_item[:node].id
-            n = Node.where(:parent_id => parent_id, :content_type => item[:controller].to_s, :content_string => content_string ).first
-            if !n
-              create = true
-            end
-          end
-        end
-
-        if create
-          n = Node.create!(:name => slug, :content_type => item[:controller].to_s, :content_string => content_string, :parent_id => parent_id, :slug => slug)
-        end
-
-        item[:node] = n
-        tree[url] = item
-
-      end
+      raise "Missing content object" if content_object.nil?
+      return "#{content_type}_controller".classify.constantize
     end
 
     def self.get_object_from_path path, params = {}
