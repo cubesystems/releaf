e865682c9bdfd2b903cea535f563a014077bb388
miks <miks@cubesystems.lv>
2013-01-31 23:00:59 +0200

    Fix locales problem with 1.9

--- templates/leaf/installer.rb
+++ templates/leaf/installer.rb
@@ -1,6 +1,10 @@
 require 'rbconfig'
 
 file 'Gemfile', <<-GEMFILE
+if RUBY_VERSION =~ /1.9/
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
+end
 
 source "https://rubygems.org"
 
