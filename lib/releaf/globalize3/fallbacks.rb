f985e7aa779e4a63566989f644ea7fe251166fb2
Aldis Berjoza <aldis@cubesystems.lv>
2013-02-25 10:30:24 +0200

    Set default and current locale if they aren;t set already

--- lib/releaf/globalize3/fallbacks.rb
+++ lib/releaf/globalize3/fallbacks.rb
@@ -3,6 +3,10 @@ module Releaf
     module Fallbacks
       def self.set
         i18n_fallbacks = {}
+
+        I18n.default_locale ||= 'en'
+        I18n.locale ||= 'en'
+
         Settings.i18n_locales + [I18n.default_locale, I18n.locale].each do |locale|
           i18n_fallbacks[locale] = Settings.i18n_locales - [locale]
         end
