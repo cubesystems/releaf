ec62437e120e8fdf735f11d0b9c1e31780a3fc83
Aldis Berjoza <aldis@cubesystems.lv>
2013-02-26 11:52:10 +0200

    This should make test pass again

--- lib/releaf/globalize3/fallbacks.rb
+++ lib/releaf/globalize3/fallbacks.rb
@@ -7,8 +7,10 @@ module Releaf
         I18n.default_locale ||= 'en'
         I18n.locale ||= 'en'
 
-        Settings.i18n_locales + [I18n.default_locale, I18n.locale].each do |locale|
-          i18n_fallbacks[locale] = Settings.i18n_locales - [locale]
+        valid_locales = Settings.i18n_locales || []
+
+        (valid_locales + [I18n.default_locale, I18n.locale]).uniq.each do |locale|
+          i18n_fallbacks[locale] = valid_locales - [locale]
         end
         Globalize.fallbacks = i18n_fallbacks
       end
