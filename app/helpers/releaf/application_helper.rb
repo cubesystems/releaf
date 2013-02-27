766acdf786fae00cecd230dfe4f14b2742f53b80
Aldis Berjoza <aldis@cubesystems.lv>
2013-02-27 16:28:10 +0200

    Fix item_to_text helper

--- app/helpers/releaf/application_helper.rb
+++ app/helpers/releaf/application_helper.rb
@@ -1,7 +1,7 @@
 module Releaf
   module ApplicationHelper
     def item_to_text item
-      item.respond_to? :to_text ? item.to_text : item.to_s
+      return item.respond_to?(:to_text) ? item.to_text : item.to_s
     end
   end
 end
