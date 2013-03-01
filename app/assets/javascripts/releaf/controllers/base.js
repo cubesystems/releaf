e077afe85c0535cc28c4f2066bbb8500be54c0a1
Aldis Berjoza <aldis@cubesystems.lv>
2013-03-01 17:54:55 +0200

    Improve datefields

--- app/assets/javascripts/releaf/controllers/base.js
+++ app/assets/javascripts/releaf/controllers/base.js
@@ -120,46 +120,6 @@ jQuery(function()
 
 
 
-    // initialize date/datetime/time pickers
-    jQuery(document.body).delegate('form', 'initcalendars', function() {
-        var forms = jQuery(this);
-        var options = {
-            timeFormat: 'HH:mm:ss',
-            controlType: 'select',
-            showHour: true,
-            showMinute: true,
-            // showSecond: true,
-            // showTimezone: true,
-        }
-
-        forms.find('.date_picker').each(function() {
-            var picker = jQuery(this);
-            var opt = options;
-            opt['dateFormat'] = picker.attr('data-date-format') || 'yy-mm-dd';
-            picker.datepicker(opt);
-        });
-
-        forms.find('.datetime_picker').each(function() {
-            var picker = jQuery(this);
-            var opt = options;
-            opt['dateFormat'] = picker.attr('data-date-format') || 'yy-mm-dd';
-            opt['pickerTimeFormat'] = picker.attr('data-time-format') || 'HH:mm'
-            picker.datetimepicker(opt);
-        });
-
-        /*
-         * forms.find('.time_picker').each(function() {
-         *     var picker = jQuery(this);
-         *     var opt = options;
-         *     opt['pickerTimeFormat'] = picker.attr('data-time-format') || 'HH:mm'
-         *     picker.timepicker(options);
-         * });
-         */
-    });
-
-    jQuery('form').trigger('initcalendars');
-
-
     jQuery('form').on('keyup, change', '.sync_field', function(e) {
         var input = jQuery(e.target);
         var value_div = input.parents('.value:first');
