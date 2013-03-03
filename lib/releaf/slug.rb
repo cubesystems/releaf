e848056413e96da4e8e63c81c7df1f56cbc8cee2
Aldis Berjoza <graudeejs@yandex.ru>
2013-03-03 13:15:33 +0200

    Refactor slug module and add documentation

--- lib/releaf/slug.rb
+++ lib/releaf/slug.rb
@@ -1,68 +1,101 @@
 module Releaf
   module Slug
-    def self.included(base)
-      base.class_eval {
+    module ClassMethods
 
-        def self.scoped_for_find_by_slug obj, scope_name=nil, scope_args=nil
-          unless scope_name.blank?
-            if scope_args.nil?
-              obj.send(scope_name.to_sym)
-            else
-              obj.send(scope_name.to_sym, *scope_args)
-            end
-          else
-            obj
-          end
+      # same as find_object, except that it will raise
+      # ActiveRecord::RecordNotFound error if no resource was found.
+      def find_object! id_or_slug, scope_name=nil, scope_args=nil
+        obj = find_object(id_or_slug, scope_name, scope_args)
+        raise ActiveRecord::RecordNotFound  unless obj
+        return obj
+      end
+
+      # Find object by slug or id.
+      #
+      # If id_or_slug looks like id, then tries to find by id first
+      # Otherwise it will search by slug field.
+      #
+      # If instance responds to children method, then it's possible to search
+      # for hierarchic resources
+      #
+      # @param id_or_slug either id of object to find (can be String or Fixnum) or slug (string) slug, may contain slash ('/')
+      #
+      # @param scope_name optional scope_name to be used for searching. This is especially useful when you are searching hierarchic resources. For example you want to find 2nd level active resource.
+      #
+      # @param scope_args any arguments that are required for scope
+      #
+      # @return resource
+      def find_object id_or_slug, scope_name=nil, scope_args=nil
+        raise ArgumentError, "id_or_slug must be String or Fixnum" unless id_or_slug.is_a?(String) or id_or_slug.is_a?(Fixnum)
+
+        unless column_names.include?('slug')
+          return scoped_for_find_by_slug(self, scope_name, scope_args).find(id_or_slug)
         end
 
-
-        def self.find_object! id_or_slug, scope_name=nil, scope_args=nil
-          self.find_object(id_or_slug, scope_name, scope_args) || raise(ActiveRecord::RecordNotFound)
+        # if it looks like id, search by id first
+        if id_or_slug.to_s =~ /\A\d+\z/
+          obj = scoped_for_find_by_slug(self, scope_name, scope_args).find_by_id(id_or_slug)
+          return obj if obj
         end
 
-        def self.find_object id_or_slug, scope_name=nil, scope_args=nil
-          raise ArgumentError, "id_or_slug must be String or Fixnum" unless id_or_slug.is_a?(String) or id_or_slug.is_a?(Fixnum)
+        unless column_names.include?('ancestry') || self.new.respond_to?(:children)
+          return scoped_for_find_by_slug(self, scope_name, scope_args).find_by_slug(id_or_slug)
+        else
+          slugs = id_or_slug.split('/')
 
-          unless self.column_names.include?('slug')
-            return scoped_for_find_by_slug(self, scope_name, scope_args).find(id_or_slug)
-          end
-
-          # if it looks like id, search by id first
-          if id_or_slug.to_s =~ /\A\d+\z/
-            obj = scoped_for_find_by_slug(self, scope_name, scope_args).find_by_id(id_or_slug)
-            return obj if obj
-          end
-
-          unless self.column_names.include?('ancestry') || self.new.respond_to?(:children)
-            return scoped_for_find_by_slug(self, scope_name, scope_args).find_by_slug(id_or_slug)
-          else
-            slugs = id_or_slug.split('/')
-
-            obj = scoped_for_find_by_slug(self, scope_name, scope_args).find_by_slug( slugs.shift )
+          obj = scoped_for_find_by_slug(self, scope_name, scope_args).find_by_slug( slugs.shift )
+          return nil unless obj
+          slugs.each do |slug_part|
+            obj = scoped_for_find_by_slug(obj.children, scope_name, scope_args).find_by_slug( slug_part )
             return nil unless obj
-            slugs.each do |slug_part|
-              obj = scoped_for_find_by_slug(obj.children, scope_name, scope_args).find_by_slug( slug_part )
-              return nil unless obj
-            end
-            return obj
           end
+          return obj
         end
+      end
 
-        def to_param
-          return id unless self.class.column_names.include?('slug')
-          return id if self.slug.blank?
-          col_names = self.class.column_names
+      private
 
-          if col_names.include?('ancestry')
-            return path.pluck(:slug).join('/')
-          elsif self.respond_to?(:parent) && parent
-            return parent.to_param + '/' + slug
+      def scoped_for_find_by_slug obj, scope_name=nil, scope_args=nil
+        unless scope_name.blank?
+          if scope_args.nil?
+            obj.send(scope_name.to_sym)
           else
-            return slug
+            obj.send(scope_name.to_sym, *scope_args)
           end
+        else
+          obj
         end
-      }
+      end
 
     end
+
+
+    module InstanceMethods
+      # Ovverrides to_param method to prefer slug field over id (when
+      # possible).  It will also generate hearachical uri part if instance
+      # supports parrent method, or has ancestry column (ancestry gem)
+      #
+      # @return Fixnum or String
+      def to_param
+        return id unless self.class.column_names.include?('slug')
+        return id if self.slug.blank?
+        col_names = self.class.column_names
+
+        if col_names.include?('ancestry')
+          return path.pluck(:slug).join('/')
+        elsif self.respond_to?(:parent) && parent
+          return parent.to_param + '/' + slug
+        else
+          return slug
+        end
+      end
+    end
+
+
+    def self.included base
+      base.extend ClassMethods
+      include InstanceMethods
+    end
+
   end
 end
