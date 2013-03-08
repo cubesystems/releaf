20eff988689b4f301e04c67f9b55a422052e5132
Aldis Berjoza <aldis@cubesystems.lv>
2013-03-08 14:49:25 +0200

    Get rid of routes in releaf initializer

--- lib/releaf/resources.rb
+++ lib/releaf/resources.rb
@@ -1,74 +1,101 @@
 require 'action_dispatch/routing/mapper'
 
-module ActionDispatch::Routing::Mapper::Resources
-  def releaf_resources(*args, &block)
-    resources *args do
-      yield if block_given?
+module ActionDispatch::Routing
+  class Mapper
 
-      add_urls = true
-      add_confirm_destroy = true
+    def releaf_resources(*args, &block)
+      resources *args do
+        yield if block_given?
+
+        add_urls = true
+        add_confirm_destroy = true
+
+        if args.last.is_a? Hash
+          options = args.last
+          if options.has_key? :only
+            add_urls            = false unless options[:only].include? :urls
+            add_confirm_destroy = false unless options[:only].include? :destroy
+          end
+
+          if options.has_key? :except
+            add_urls            = false if options[:except].include? :urls
+            add_confirm_destroy = false if options[:except].include? :destroy
+          end
+        end
+
+        match :urls,            :on => :collection  if add_urls
+        get   :confirm_destroy, :on => :member      if add_confirm_destroy
+      end
+    end
+
+    def slugged_resources(*args, &block)
+      add_routes = {
+        :new      => true,
+        :create   => true,
+        :edit     => true,
+        :update   => true,
+        :show     => true,
+        :index    => true,
+        :destroy  => true
+      }
+
+      new_resources = args
 
       if args.last.is_a? Hash
-        options = args.last
+
+        options = new_resources.pop
+
         if options.has_key? :only
-          add_urls            = false unless options[:only].include? :urls
-          add_confirm_destroy = false unless options[:only].include? :destroy
+          add_routes.each_key do |key|
+            add_routes[key] = false unless options[:only].include? key
+          end
         end
 
         if options.has_key? :except
-          add_urls            = false if options[:only].include? :urls
-          add_confirm_destroy = false if options[:only].include? :destroy
+          add_routes.each_key do |key|
+            add_routes[key] = false if options[:only].include? key
+          end
         end
+
       end
 
-      match :urls,            :on => :collection  if add_urls
-      get   :confirm_destroy, :on => :member      if add_confirm_destroy
-    end
-  end
-
-  def slugged_resources(*args, &block)
-    add_routes = {
-      :new      => true,
-      :create   => true,
-      :edit     => true,
-      :update   => true,
-      :show     => true,
-      :index    => true,
-      :destroy  => true
-    }
-
-    new_resources = args
-
-    if args.last.is_a? Hash
-
-      options = new_resources.pop
-
-      if options.has_key? :only
-        add_routes.each_key do |key|
-          add_routes[key] = false unless options[:only].include? key
-        end
-      end
-
-      if options.has_key? :except
-        add_routes.each_key do |key|
-          add_routes[key] = false if options[:only].include? key
-        end
+      new_resources.each do |resource_name|
+        res_name = resource_name.to_s
+        get     res_name,                  :to => "#{res_name}#index",       :as => res_name                          if add_routes[:index]
+        post    res_name,                  :to => "#{res_name}#create"                                                if add_routes[:create]
+        get     "#{res_name}/new",         :to => "#{res_name}#new",         :as => "new_#{res_name.singularize}"     if add_routes[:new]
+        yield if block_given?
+        get     "#{res_name}/*slug/edit",  :to => "#{res_name}#edit",        :as => "edit_#{res_name.singularize}"    if add_routes[:edit]
+        get     "#{res_name}/*slug",       :to => "#{res_name}#show",        :as => "show_#{res_name.singularize}"    if add_routes[:show]
+        put     "#{res_name}/*slug",       :to => "#{res_name}#update"                                                if add_routes[:update]
+        post    "#{res_name}/*slug",       :to => "#{res_name}#create"                                                if add_routes[:create]
+        delete  "#{res_name}/*slug",       :to => "#{res_name}#destroy"                                               if add_routes[:destroy]
       end
 
     end
 
-    new_resources.each do |resource_name|
-      res_name = resource_name.to_s
-      get     res_name,                  :to => "#{res_name}#index",       :as => res_name                          if add_routes[:index]
-      post    res_name,                  :to => "#{res_name}#create"                                                     if add_routes[:create]
-      get     "#{res_name}/new",         :to => "#{res_name}#new",         :as => "new_#{res_name.singularize}"     if add_routes[:new]
-      yield if block_given?
-      get     "#{res_name}/*slug/edit",  :to => "#{res_name}#edit",        :as => "edit_#{res_name.singularize}"    if add_routes[:edit]
-      get     "#{res_name}/*slug",       :to => "#{res_name}#show",        :as => "show_#{res_name.singularize}"    if add_routes[:show]
-      put     "#{res_name}/*slug",       :to => "#{res_name}#update"                                                     if add_routes[:update]
-      post    "#{res_name}/*slug",       :to => "#{res_name}#create"                                                     if add_routes[:create]
-      delete  "#{res_name}/*slug",       :to => "#{res_name}#destroy"                                                    if add_routes[:destroy]
+    def mount_releaf_at mount_location
+
+      post '/tinymce_assets' => 'releaf/tinymce_assets#create'
+
+      devise_for Releaf.devise_for, :path => mount_location, :controllers => { :sessions => "releaf/sessions" }
+
+      scope mount_location do
+        namespace :releaf, :path => nil do
+
+          releaf_resources :admins, :roles
+
+          releaf_resources :nodes, :controller => "content", :path => "content" do
+            get :generate_url, :on => :collection
+          end
+
+          releaf_resources :translation_groups, :controller => "translations", :path => "translations", :except => [:show], :path_names => { :edit => '' }
+
+          root :to => "content#index"
+        end
+      end
     end
 
-  end
-end
+
+  end # class Mapper
+end # module ActionDispatch::Routing
