feb6aafe4b33cc54533917ff0da926f7db358544
Aldis Berjoza <aldis@cubesystems.lv>
2013-03-11 10:04:01 +0200

    Add missing field to create_releaf_tinymce_assets migration

--- lib/generators/releaf/templates/migrations/create_releaf_tinymce_assets.rb
+++ lib/generators/releaf/templates/migrations/create_releaf_tinymce_assets.rb
@@ -3,6 +3,7 @@ class CreateReleafTinymceAssets < ActiveRecord::Migration
     create_table :releaf_tinymce_assets do |t|
       t.string :file_uid
       t.string :file_name
+      t.string :file_type
 
       t.timestamps
     end
