86be286148bf89d9e819fc294daa6ddb877647bf
miks <miks@cubesystems.lv>
2013-03-29 21:43:48 +0200

    View access denied error

--- app/controllers/releaf/base_application_controller.rb
+++ app/controllers/releaf/base_application_controller.rb
@@ -4,6 +4,8 @@ module Releaf
     before_filter "authenticate_#{ReleafDeviseHelper.devise_admin_model_name}!"
     before_filter :set_locale
 
+    rescue_from Releaf::AccessDenied, :with => :access_denied
+
     # check_authorization :unless => :devise_controller?
     layout Releaf.layout
     protect_from_forgery
@@ -18,5 +20,9 @@ module Releaf
       Releaf::Globalize3::Fallbacks.set
     end
 
+    def access_denied
+      @controller_name = full_controller_name
+      render :template => 'error_pages/access_denied'
+    end
   end
 end
