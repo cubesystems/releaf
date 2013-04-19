c19a9349933ed4a074c893852a56cfc86110a394
miks <miks@cubesystems.lv>
2013-04-19 22:13:30 +0300

    Better role authorize tests

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -40,6 +40,29 @@ describe Releaf::Role do
     before do
       @admin_role = FactoryGirl.create(:admin_role)
       @content_role = FactoryGirl.create(:content_role)
+      @role_without_permissions = FactoryGirl.create(:content_role, :permissions => [])
+    end
+
+    context "when arguments that are neither String or class that inherit ActionController::Base given" do
+      it "should raise ArgumentError" do
+        expect{ @content_role.authorize!([]) }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when access to releaf/home controller authorized" do
+      it "should always return true" do
+        @admin_role.authorize!(Releaf::HomeController.new).should be_true
+        @content_role.authorize!(Releaf::HomeController.new).should be_true
+        @role_without_permissions.authorize!(Releaf::HomeController.new).should be_true
+      end
+    end
+
+    context "when access to releaf/tinymce_assets controller authorized" do
+      it "should always return true if role has permissions to access releaf/content controller" do
+        @admin_role.authorize!(Releaf::TinymceAssetsController.new).should be_true
+        @content_role.authorize!(Releaf::TinymceAssetsController.new).should be_true
+        @role_without_permissions.authorize!(Releaf::TinymceAssetsController.new).should be_false
+      end
     end
 
     context "when permissions given" do
@@ -47,12 +70,14 @@ describe Releaf::Role do
         @admin_role.authorize!(Releaf::TranslationsController.new).should be_true
         @admin_role.authorize!(Releaf::ContentController.new).should      be_true
         @content_role.authorize!(Releaf::ContentController.new).should    be_true
+        @role_without_permissions.authorize!(Releaf::ContentController.new).should    be_false
       end
     end
 
     context "when permissions not given" do
       it "returns false" do
         @content_role.authorize!(Releaf::TranslationsController.new).should be_false
+        @role_without_permissions.authorize!(Releaf::TranslationsController.new).should be_false
       end
     end
   end
