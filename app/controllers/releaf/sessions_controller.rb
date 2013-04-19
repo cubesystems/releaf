55136a3d570c83448f54fb150254e06fcd6a8e3b
miks <miks@cubesystems.lv>
2013-04-19 09:44:27 +0300

    Redirecting for now is handled by home controller

--- app/controllers/releaf/sessions_controller.rb
+++ app/controllers/releaf/sessions_controller.rb
@@ -1,19 +1,5 @@
 module Releaf
   class SessionsController < Devise::SessionsController
     layout "releaf/devise"
-
-    def after_sign_in_path_for(resource)
-      controller_name = resource.role.default_controller
-      controller = Releaf.controller_list[controller_name]
-
-      if controller.has_key? :helper 
-        url = send(controller[:helper] + "_path")
-      else
-        url = send(controller[:controller].gsub('/', '_') + "_path")
-      end
-
-      return url
-    end
-
   end
 end
