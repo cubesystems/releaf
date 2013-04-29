c659fc72b016ee3dd133b476a216821c2fbe740f
Aldis Berjoza <graudeejs@yandex.ru>
2013-04-29 21:49:39 +0300

    Automagically map #edit to #show path if #show is not used

--- lib/releaf/resources.rb
+++ lib/releaf/resources.rb
@@ -4,24 +4,38 @@ module ActionDispatch::Routing
   class Mapper
 
     def releaf_resources(*args, &block)
-      resources *args do
-        yield if block_given?
+      add_urls = true
+      add_confirm_destroy = true
 
-        add_urls = true
-        add_confirm_destroy = true
+      if args.last.is_a? Hash
+        options = args.last
+        if options.has_key? :only
+          add_urls            = false unless options[:only].include? :urls
+          add_confirm_destroy = false unless options[:only].include? :destroy
 
-        if args.last.is_a? Hash
-          options = args.last
-          if options.has_key? :only
-            add_urls            = false unless options[:only].include? :urls
-            add_confirm_destroy = false unless options[:only].include? :destroy
-          end
-
-          if options.has_key? :except
-            add_urls            = false if options[:except].include? :urls
-            add_confirm_destroy = false if options[:except].include? :destroy
+          unless options[:only].include? :show
+            if options[:only].include? :edit
+              options = { :path_names => { :edit => '' } }.deep_merge(options)
+            end
           end
         end
+
+        if options.has_key? :except
+          add_urls            = false if options[:except].include? :urls
+          add_confirm_destroy = false if options[:except].include? :destroy
+
+          if options[:except].include? :show
+            unless options[:except].include? :edit
+              options = { :path_names => { :edit => '' } }.deep_merge(options)
+            end
+          end
+        end
+        args[-1] = options
+
+      end
+
+      resources *args do
+        yield if block_given?
 
         match :urls,            :on => :collection  if add_urls
         get   :confirm_destroy, :on => :member      if add_confirm_destroy
@@ -41,35 +55,52 @@ module ActionDispatch::Routing
 
       new_resources = args
 
+      edit_instead_of_show = false
+
       if args.last.is_a? Hash
 
-        options = new_resources.pop
+        options = args.pop
 
         if options.has_key? :only
           add_routes.each_key do |key|
             add_routes[key] = false unless options[:only].include? key
           end
+
+          unless options[:only].include? :show
+            if options[:only].include? :edit
+              edit_instead_of_show = true
+            end
+          end
         end
 
         if options.has_key? :except
           add_routes.each_key do |key|
-            add_routes[key] = false if options[:only].include? key
+            add_routes[key] = false if options[:except].include? key
+          end
+
+          if options[:except].include? :show
+            unless options[:except].include? :edit
+              edit_instead_of_show = true
+            end
           end
         end
-
       end
 
       new_resources.each do |resource_name|
         res_name = resource_name.to_s
-        get     res_name,                  :to => "#{res_name}#index",       :as => res_name                          if add_routes[:index]
-        post    res_name,                  :to => "#{res_name}#create"                                                if add_routes[:create]
-        get     "#{res_name}/new",         :to => "#{res_name}#new",         :as => "new_#{res_name.singularize}"     if add_routes[:new]
+        get     res_name,                   :to => "#{res_name}#index",       :as => res_name                          if add_routes[:index]
+        post    res_name,                   :to => "#{res_name}#create"                                                if add_routes[:create]
+        get     "#{res_name}/new",          :to => "#{res_name}#new",         :as => "new_#{res_name.singularize}"     if add_routes[:new]
         yield if block_given?
-        get     "#{res_name}/*slug/edit",  :to => "#{res_name}#edit",        :as => "edit_#{res_name.singularize}"    if add_routes[:edit]
-        get     "#{res_name}/*slug",       :to => "#{res_name}#show",        :as => "show_#{res_name.singularize}"    if add_routes[:show]
-        put     "#{res_name}/*slug",       :to => "#{res_name}#update"                                                if add_routes[:update]
-        post    "#{res_name}/*slug",       :to => "#{res_name}#create"                                                if add_routes[:create]
-        delete  "#{res_name}/*slug",       :to => "#{res_name}#destroy"                                               if add_routes[:destroy]
+        if edit_instead_of_show
+          get     "#{res_name}/*slug",      :to => "#{res_name}#edit",        :as => "edit_#{res_name.singularize}"    if add_routes[:edit]
+        else
+          get     "#{res_name}/*slug/edit", :to => "#{res_name}#edit",        :as => "edit_#{res_name.singularize}"    if add_routes[:edit]
+          get     "#{res_name}/*slug",      :to => "#{res_name}#show",        :as => "show_#{res_name.singularize}"    if add_routes[:show]
+        end
+        put     "#{res_name}/*slug",        :to => "#{res_name}#update"                                                if add_routes[:update]
+        post    "#{res_name}/*slug",        :to => "#{res_name}#create"                                                if add_routes[:create]
+        delete  "#{res_name}/*slug",        :to => "#{res_name}#destroy"                                               if add_routes[:destroy]
       end
 
     end
