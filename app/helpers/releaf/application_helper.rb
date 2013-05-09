22f22055ca5775fe3a3912a3bf9681b9f0dcf387
Aldis Berjoza <aldis@cubesystems.lv>
2013-05-09 10:47:54 +0300

    add i18n_options_for_select helper

--- app/helpers/releaf/application_helper.rb
+++ app/helpers/releaf/application_helper.rb
@@ -1,4 +1,28 @@
 module Releaf
   module ApplicationHelper
+    def i18n_options_for_select container, selected, prefix, i18n_options={}
+      i18n_options = { :scope => controller_scope_name }.merge(i18n_options)
+
+      if container.is_a? Hash
+        translated_array = []
+
+        hash.each_pair do |key, value|
+          translated_item = I18n.t("#{prefix.to_s}-#{value.to_s}", i18n_options.merge(:default => key.to_s))
+          [translated_item, value]
+        end
+
+        return translated_array
+
+      elsif container.is_a? Array
+        translated_array = container.map do |item|
+          translated_item = I18n.t("#{prefix.to_s}-#{item.to_s}", i18n_options.merge(:default => item.to_s))
+          [translated_item, item.to_s]
+        end
+        return translated_array
+      else
+        raise ArgumentError, "unsupported container: #{container.class.name}"
+      end
+    end
+
   end
 end
