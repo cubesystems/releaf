b0a69aca64b904c4b01dddb1b041c0a0472c2151
miks <miks@cubesystems.lv>
2013-05-16 10:40:28 +0300

    Do not reload translations on translation data save

--- lib/i18n/backend/releaf/translation_data.rb
+++ lib/i18n/backend/releaf/translation_data.rb
@@ -19,19 +19,9 @@ module I18n
 
         scope :available_locales, where('lang IS NOT NULL').group(:lang).pluck(:lang)
 
-        after_commit :reload_cache
-
         def text
           self[:localization].blank? == false ? self[:localization] : self.translation.try(:key).try(:humanize)
         end
-
-        private
-
-        def reload_cache
-          Settings.i18n_updated_at = Time.now
-          I18n.backend.reload_cache
-        end
-
       end
     end
   end
