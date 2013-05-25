b2d2a54c6a44998f46e914dc0b0044fb8b3c8e37
miks <miks@cubesystems.lv>
2013-05-26 01:15:37 +0300

    application helper spec added

--- app/helpers/releaf/application_helper.rb
+++ app/helpers/releaf/application_helper.rb
@@ -3,35 +3,22 @@ module Releaf
     def i18n_options_for_select container, selected, prefix, i18n_options={}
       i18n_options = { :scope => controller_scope_name }.merge(i18n_options)
 
-
       translated_array = []
 
       if container.is_a? Hash
-
-        hash.each_pair do |key, value|
-          translated_item = I18n.t("#{prefix.to_s}-#{value.to_s}", i18n_options.merge(:default => key.to_s))
-          translated_array += [translated_item, value]
+        container.each_pair do |key, hash_value|
+          text = I18n.t("#{prefix.to_s}-#{hash_value.to_s}", i18n_options.merge(:default => hash_value.to_s))
+          value = hash_value.respond_to?(:id) ? hash_value.id : key.to_s
+          translated_array << [text, value]
         end
-
       elsif container.is_a? Array
-        if container.first.respond_to?(:id)
-          translated_array = container.map do |item|
-            [
-              I18n.t("#{prefix.to_s}-#{item.respond_to?(:to_text) ? item.to_text : item.to_s}", i18n_options.merge(:default => item.respond_to?(:to_text) ? item.to_text : item.to_s)),
-              item.id
-            ]
-          end
-
-        else
-          translated_array = container.map do |item|
-            [
-              I18n.t("#{prefix.to_s}-#{item.respond_to?(:to_text) ? item.to_text : item.to_s}", i18n_options.merge(:default => item.respond_to?(:to_text) ? item.to_text : item.to_s)),
-              item.to_s
-            ]
-          end
+        container.each do |item|
+          text = I18n.t("#{prefix.to_s}-#{item.to_s}", i18n_options.merge(:default => item.to_s))
+          value = item.respond_to?(:id) ? item.id : item.to_s
+          translated_array << [text, value]
         end
       else
-        raise ArgumentError, "unsupported container: #{container.class.name}"
+        raise ArgumentError, "unsupported container class: #{container.class.name}"
       end
 
       return options_for_select(translated_array, selected)
