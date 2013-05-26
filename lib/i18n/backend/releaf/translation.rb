3b88478b69bca9e397ef14fd8f4cc3d80c122bee
miks <miks@cubesystems.lv>
2013-05-26 12:33:55 +0300

    Translation model spec added

--- lib/i18n/backend/releaf/translation.rb
+++ lib/i18n/backend/releaf/translation.rb
@@ -8,7 +8,7 @@ module I18n
         self.table_name = "releaf_translations"
 
         validates_presence_of :group_id, :key
-        validates_uniqueness_of :key, :scope => :group_id
+        validates_uniqueness_of :key
 
         belongs_to :translation_group, :foreign_key => :group_id
         has_many :translation_data, :dependent => :destroy, :class_name => 'Releaf::TranslationData'
@@ -17,14 +17,6 @@ module I18n
           :group_id,
           :key
 
-        scope :joined, select('releaf_translations.*, releaf_translation_data.lang as "lang", releaf_translation_data.localization as "localization", releaf_translation_groups.scope as "scope"').
-          joins(:translation_data, :translation_group)
-
-        scope :get_translated, joined.where('releaf_translation_data.lang IS NOT NULL');
-
-        scope :lookup, lambda { |locale, keys, scope|
-          joined.where('releaf_translation_groups.scope = :scope AND releaf_translation_data.lang = :locale AND releaf_translations.key in (:keys)', :keys => keys, :locale => locale, :scope => scope)
-        }
         scope :filter, lambda{ |params|
           translations_join = 'LEFT OUTER JOIN `releaf_translation_data` ON `releaf_translations`.`id` = `releaf_translation_data`.`translation_id`'
           where( '(releaf_translations.key LIKE ? OR releaf_translation_data.localization LIKE ?)', "%#{params[:search]}%", "%#{params[:search]}%" ).joins(translations_join).group('releaf_translations.id') unless params[:search].blank?
@@ -50,7 +42,6 @@ module I18n
         def plain_key
           key.gsub(translation_group.scope + '.', '')
         end
-
       end
     end
   end
---
+++ spec/models/translation.rb
@@ -0,0 +1,77 @@
+# encoding: UTF-8
+
+require "spec_helper"
+
+describe I18n::Backend::Releaf::Translation do
+
+  it { should have(1).error_on(:group_id) }
+  it { should have(1).error_on(:key) }
+  it {
+    FactoryGirl.create(:translation)
+    should validate_uniqueness_of(:key)
+  }
+  it { should belong_to(:translation_group) }
+
+
+  before do
+    Settings.i18n_locales = ["de", "en"]
+    Settings.i18n_admin_locales = ["lv"]
+
+    @group = FactoryGirl.create(:translation_group, :scope => 'test')
+    @translation = FactoryGirl.create(:translation, :translation_group => @group, :key => 'test.apple')
+    FactoryGirl.create(:translation_data, :localization => 'apple', :translation => @translation, :lang => "en")
+    FactoryGirl.create(:translation_data, :localization => 'apfel', :translation => @translation, :lang => "de")
+
+    Settings.i18n_updated_at = Time.now
+  end
+
+  describe "scope: filter" do
+    context "when filtering with 'apfel'" do
+      it "returns 1 translation" do
+        I18n::Backend::Releaf::Translation.filter(:search => "apfel").length.should eq(1)
+      end
+    end
+
+    context "when filtering with 'ap' (group by test)" do
+      it "returns 1 translation" do
+        I18n::Backend::Releaf::Translation.filter(:search => "ap").length.should eq(1)
+      end
+    end
+
+    context "when filtering with 'test.apple'" do
+      it "returns 1 translation" do
+        I18n::Backend::Releaf::Translation.filter(:search => "test.apple").length.should eq(1)
+      end
+    end
+
+    context "when filtering with 'asdsad'" do
+      it "returns 0 translation" do
+        I18n::Backend::Releaf::Translation.filter(:search => "asdsad").length.should eq(0)
+      end
+    end
+  end
+
+  describe "#locales" do
+    it "should return translated data values in hash" do
+      @translation.locales.should eq({"en" => "apple", "de" => "apfel", "lv" => nil})
+    end
+  end
+
+  describe "#plain_key" do
+    it "should return plain key without group scope" do
+      @translation.plain_key.should eq('apple')
+    end
+  end
+
+  describe "translation data model relation" do
+    it "should have relation to translation data" do
+      @translation.translation_data.size.should eq(2)
+    end
+
+    it "should destroy translation data by destroying translation itself" do
+      I18n::Backend::Releaf::TranslationData.all.count.should eq(2)
+      @translation.destroy
+      I18n::Backend::Releaf::TranslationData.all.count.should eq(0)
+    end
+  end
+end
