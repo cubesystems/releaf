77e0db51aa15a67dc0288486c80b2ce56ac08f58
uldis <uldis@cubesystems.lv>
2013-07-15 14:05:14 +0300

    Breadcrumbs helper

--- app/helpers/releaf/admin_helper.rb
+++ app/helpers/releaf/admin_helper.rb
@@ -5,6 +5,37 @@ module Releaf
       self.send("current_#{ReleafDeviseHelper.devise_admin_model_name}")
     end
 
+    def admin_breadcrumbs resource = nil
+      breadcrumbs = []
+      breadcrumbs << { :name => I18n.t('Home', :scope => 'admin.breadcrumbs'), :url => releaf_root_path }
+      
+      admin_main_menu.each do |item|
+        if item[:active]
+          breadcrumbs << {:name => I18n.t(item[:name], :scope => "admin.menu_items"), :url => item[:url]}
+          unless item[:items].blank?
+            item[:items].each do |sub_menu_item|
+              if sub_menu_item[:active]
+                breadcrumbs << { :name => I18n.t(sub_menu_item[:name], :scope => "admin.menu_items"), :url => sub_menu_item[:url] }
+              end
+            end
+          end
+        end
+      end
+
+      unless resource.nil?
+        if resource.new_record?
+          breadcrumbs << { :name => I18n.t('New record', :scope => 'admin.breadcrumbs') }
+        elsif resource.respond_to?(:to_text)
+          breadcrumbs << {:name => resource.send(:to_text)}
+        else
+          breadcrumbs << { :name => I18n.t('Edit record', :scope => 'admin.breadcrumbs') }
+        end
+      end
+
+      return breadcrumbs
+    end
+
+
     def admin_main_menu
       items = []
 
