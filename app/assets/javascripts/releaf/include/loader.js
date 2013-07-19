e98aacccc9b79a7aebf535ad32e1eda4b47de60e
gnc <guntis@cubesystems.lv>
2013-07-19 17:22:24 +0300

    disable buttons with loader

--- app/assets/javascripts/releaf/include/loader.js
+++ app/assets/javascripts/releaf/include/loader.js
@@ -9,10 +9,15 @@ jQuery(function()
         {
             return;
         }
+        
         button.addClass('loading');
         
+        button.data('disabled-before-loading', button.prop('disabled') );
+        
+        button.prop('disabled', true);
+        button.addClass( 'disabled' );
+        
         var loader = jQuery('<i />').addClass('loader icon-spin icon-spinner');
-            
         button.append( loader );
     });
     
@@ -22,6 +27,17 @@ jQuery(function()
         var button = jQuery(e.target);
         
         button.find('.loader').remove();
+
+        var disabled_before_loading = button.data('disabled-before-loading') ;
+        
+        if (typeof disabled_before_loading != 'undefined')
+        {
+            if (!disabled_before_loading)
+            {
+                button.removeClass('disabled');
+                button.prop('disabled', false);
+            }
+        }
 
         button.removeClass('loading');
 
