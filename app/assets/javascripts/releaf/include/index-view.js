a9856e2f580c991cf955c7eae0c3fd08b41ef03c
gnc <guntis@cubesystems.lv>
2013-07-19 12:19:19 +0300

    event-based index search script

--- app/assets/javascripts/releaf/include/index-view.js
+++ app/assets/javascripts/releaf/include/index-view.js
@@ -1,78 +1,162 @@
 jQuery(function()
 {
-	// ajax search
-	jQuery( '.view-index' ).on( 'searchinit', function( event )
-	{
-		var form = jQuery( event.target );
+    jQuery('body').on('searchinit', 'form', function( e ) 
+    {
+        var form = jQuery(e.target);
 
-		var input = form.find( 'input[name="search"]' );
-		var timeout;
-		var request;
-		var last_search_query = input.val();
+        var input   = form.find('[name="search"]');
+                
+        var request;
+        var timeout;
 
-		var main  = form.parents( 'body > .main' );
-		var header   = main.children( '.header' );
-        // sometime all index can be inside form (because of checkbox/etc),
-        // so use "find" method
-		var table   = main.find( '.table' );
-		var footer = main.children( 'footer' );
+        var last_query  = input.val();
+        
+        var options = form.data('search-options');
 
-		// custom "lookup" event allows to issue extra reloads from the outside
+        if (typeof options == 'undefined')
+        {
+            options = {};
+        }
+        
+        if (typeof options.result_blocks == 'undefined')
+        {
+            // define default html blocks that will be fetched from search response
+            // and placed in page body
+            options.result_blocks = 
+            {
+                header : 
+                { 
+                    result_selector : '.header',
+                    target : jQuery('body > .main .header').first()
+                },
 
-		form.bind( 'lookup', function()
-		{
-			// cancel previous timeout
-			clearTimeout( timeout );
-			// cancel previous unfinished request
-			if( request !== undefined )
-			{
-				request.abort();
-			}
-			timeout = setTimeout(function()
-			{
-				// set loading icon
-				var submit_button = form.find('button[type="submit"]');
-                submit_button.addClass( 'loading' );
-				// construct url
-				var url = new RequestUrl( form.attr( 'action' ) );
-				url.add( form.serializeArray() );
-				url.add({ ajax: 1 });
-				// send request
-				request = jQuery.ajax
-				({
-					url: url.getUrl(),
-					success: function( response )
-					{
-						// remove loading icon
-						submit_button.removeClass( 'loading' );
+                table :
+                {
+                    result_selector : '.table',
+                    target : jQuery('body > .main .table').first()
+                },
 
-						// create html holder
-                        var html = jQuery( '<div />' ).append( response );
+                footer :
+                {
+                    result_selector : 'footer',
+                    target : jQuery('body > .main footer').first()
+                }
+            };
+        }
+        
+ 
+        form.on( 'searchstart', function()
+        {
+            // cancel previous timeout
+            clearTimeout( timeout );
 
-                        header.html( html.find( '.header' ).html() );
-                        table.html( html.find( '.table' ).html() );
-                        footer.html( html.find( 'footer' ).html() );
+            // cancel previous unfinished request
+            if (request)
+            {
+                request.abort();
+            }
 
-                        table.trigger('toolboxinit');
-					}
-				});
-			}, 200 );
-		});
+            timeout = setTimeout(function()
+            {
+                form.addClass( 'loading' );
+                
+                // construct url
+                var url = new RequestUrl( false );
+                url.add( form.serializeArray() );
 
-		// attach keypress event
-		input.keyup( function()
-		{
-			if( input.val() == last_search_query )
-			{
-				return;
-			}
-			last_search_query = input.val();
-			form.trigger( 'lookup' );
-		});
-	});
+                if ('replaceState' in window.history)
+                {
+                    window.history.replaceState( window.history.state, window.title, url.getUrl());
+                }
 
-    // init default form
-    jQuery( '.view-index form.search' ).trigger( 'searchinit' );
+                url.add({ ajax: 1 });
+                
+                // send request
+                request = jQuery.ajax
+                ({
+                    url: url.getUrl(),
+                    success: function( response )
+                    {
+                        // remove loading icon
+                        form.removeClass( 'loading' );
+
+                        form.trigger('searchresponse', response);
+
+                        form.trigger('searchend');
+                    }
+                });
+            }, 200 );
+        }); 
+
+        
+        form.on( 'searchresponse', function(e, response)
+        {
+            var response = jQuery('<div />').append( response );
+            
+            // for each result block find its content in response 
+            // and copy it to its target container
+            
+            for (var key in options.result_blocks)
+            {
+                var block = options.result_blocks[ key ];
+                
+                var content = response.find( block.result_selector ).first().html();
+
+                jQuery( block.target ).html( content );
+                
+                block.target.trigger('contentreplaced');
+            }
+        });
+        
+        form.on( 'searchend', function( e )
+        {
+            form.removeClass( 'loading' );
+        });
+
+
+        input.on( 'keyup',  function()
+        {
+            if (input.val() == last_query )
+            {
+                return;
+            }
+            last_query = input.val();
+
+            form.trigger( 'searchstart' );
+        });
+
+
+        form.on('change', 'input, select', function( e)
+        {
+            if (
+                (input[0] == e.target)
+                &&
+                (input.val() == last_query )
+            )
+            {
+                // do not trigger searchstart on main input change
+                // if last query is the same
+                return;
+            }
+            
+            form.trigger('searchstart');
+        });
+
+
+    });
+
+    setTimeout( function () 
+    {
+        // initialize search via timeout 
+        // to allow any custom code to set custom form search options
+        
+        jQuery( '.view-index form.search' ).trigger( 'searchinit' );
+        
+    }, 0);
+
+
+
+
 
     jQuery('#page_select').on('change', function(){
         var val=jQuery(this).val();
@@ -82,4 +166,5 @@ jQuery(function()
             window.location.href = request_url;
         }
     });
+    
 });
