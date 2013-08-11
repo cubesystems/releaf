6761a938ad588d26511b843b3bee8f2b9bb9ad11
miks <miks@cubesystems.lv>
2013-08-11 13:23:48 +0300

    Do not hit database for missing translations more then one time

--- lib/i18n/backend/releaf.rb
+++ lib/i18n/backend/releaf.rb
@@ -2,6 +2,7 @@ require 'i18n/backend/base'
 
 module I18n
   module Backend
+
     class Releaf
       autoload :Translation,      'i18n/backend/releaf/translation'
       autoload :TranslationData,  'i18n/backend/releaf/translation_data'
@@ -22,33 +23,29 @@ module I18n
         def reload_cache
           I18N_CACHE.clear
 
-          query = Translation.joins(:translation_data)
-          query = query.select(["releaf_translations.key", "releaf_translation_data.lang", "releaf_translation_data.localization"])
-
-          query.each do |translation|
+          keys = ["releaf_translations.key", "releaf_translation_data.lang", "releaf_translation_data.localization"]
+          Translation.joins(:translation_data).select(keys).each do |translation|
             I18N_CACHE.write [translation.lang, translation.key], translation.localization
           end
+
           I18N_CACHE.write('UPDATED_AT', Settings.i18n_updated_at)
         end
 
         protected
 
-
-
-        def check_cache
-          return if I18N_CACHE.read('UPDATED_AT') == Settings.i18n_updated_at
-          reload_cache
-        end
-
         # Lookup translation from database
         def lookup(locale, key, scope = [], options = {})
-          check_cache
+          # reload cache if cache timestamp differs from last translations update
+          reload_cache if I18N_CACHE.read('UPDATED_AT') != Settings.i18n_updated_at
 
           if scope.blank? && key !~ /\./
             scope = DEFAULT_SCOPE
           end
 
           key = normalize_flat_keys(locale, key, scope, options[:separator])
+          # do not process further if key already marked as missing
+          return nil if I18N_CACHE.read([:missing, [locale, key]])
+
           chain = key.split('.')
           search_key = chain.pop
           keys_to_check_for_other_locales = []
@@ -57,7 +54,7 @@ module I18n
             # build full translation key with current scope
             check_key = (chain + [search_key]).join('.')
             # read value from 118N cache
-            result = I18N_CACHE.read [locale, check_key]
+            result = I18N_CACHE.read([locale, check_key])
             # store key for checking in other locales
             keys_to_check_for_other_locales.push check_key
 
@@ -70,10 +67,12 @@ module I18n
             return result unless result.blank?
           end
 
+          # mark translation as missing
+          I18N_CACHE.write([:missing, [locale, key]], true)
+
           if ::Releaf.create_missing_translations
             # do not create new translation if exists in database in any scope
             unless Translation.where('releaf_translations.key IN (?)', keys_to_check_for_other_locales).exists?
-              I18N_CACHE.write([:missing, [locale, check_key]], true)
               save_missing_translation(locale, key)
             end
           end
@@ -83,10 +82,9 @@ module I18n
 
         def save_missing_translation(locale, key)
           scope_parts = get_scope key
+          group = TranslationGroup.find_or_create_by_scope(:scope => scope_parts.join('.'))
 
-          tg = TranslationGroup.find_or_create_by_scope(:scope => scope_parts.join('.'))
-
-          tg.translations.find_or_create_by_key(key)
+          group.translations.find_or_create_by_key(key)
           I18N_CACHE.write [locale, key], false
         end
 
@@ -95,11 +93,9 @@ module I18n
           return scope unless scope.empty?
           return DEFAULT_SCOPE
         end
-
       end
 
       include Implementation
     end
   end
 end
-
