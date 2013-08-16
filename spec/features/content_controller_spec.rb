93e77dfbc2f5dd2f48c1d1a484ccf8461eddf44e
uldis <uldis@cubesystems.lv>
2013-08-16 15:24:18 +0300

    Feature tests updated fo move/copy/go content actions

--- spec/features/content_controller_spec.rb
+++ spec/features/content_controller_spec.rb
@@ -52,30 +52,10 @@ describe Releaf::ContentController do
       visit edit_releaf_node_path @node
     end
 
-    context "when clicking on toolbox button", js: true do
-      it "displays go to option in menu" do
-        find('.toolbox button').click
-        wait_for_ajax_to_complete
-        expect(page).to have_css('.toolbox-items li a.ajaxbox', text: "Go to", visible: true)
-      end
-    end
-
-    context "when presing go to link in toolbox list", js: true do
-      it "displays go to dialog" do
-        find('.toolbox button').click
-        wait_for_ajax_to_complete
-        find('.toolbox-items li a.ajaxbox', text: "Go to").click
-        wait_for_ajax_to_complete
-        expect(page).to have_css('.dialog.goto-node-dialog', visible: true)
-      end
-    end
-
     context "when going to node from toolbox list", js: true do
       it "navigates to targeted node's edit view" do
         find('.toolbox button').click
-        wait_for_ajax_to_complete
         find('.toolbox-items li a.ajaxbox', text: "Go to").click
-        wait_for_ajax_to_complete
         find('.dialog.goto-node-dialog .action-tree ul li .node-cell a', text: "Root").click
 
         expect(page).to have_css('.view-edit .edit_resource h2.header', text: 'Root', visible: true)
@@ -84,30 +64,10 @@ describe Releaf::ContentController do
   end
 
   describe "copy node to" do
-    context "when pressing toolbox icon", js: true do
-      it "displays copy link" do
-        find('li[data-id="' + @root.id.to_s + '"] > .toolbox-cell button').click
-        wait_for_ajax_to_complete
-        expect(page).to have_css('.toolbox-items li a.ajaxbox', text: "Copy", visible: true)
-      end
-    end
-
-    context "when pressing copy link", js: true do
-      it "displays copy dialog" do
-        find('li[data-id="' + @root.id.to_s + '"] > .toolbox-cell button').click
-        wait_for_ajax_to_complete
-        find('.toolbox-items li a.ajaxbox', text: "Copy").click
-        wait_for_ajax_to_complete
-        expect(page).to have_css('.dialog.copy-node-dialog', visible: true)
-      end
-    end
-
     context "when copying node", js: true do
       it "shows copied node in tree" do
         find('li[data-id="' + @node.id.to_s + '"] > .toolbox-cell button').click
-        wait_for_ajax_to_complete
         find('.toolbox-items li a.ajaxbox', text: "Copy").click
-        wait_for_ajax_to_complete
         find('.dialog.copy-node-dialog .action-tree ul li .node-cell button', text: "Root").click
         find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
 
@@ -117,41 +77,13 @@ describe Releaf::ContentController do
   end
 
   describe "move node to" do
-    context "when pressing toolbox icon", js: true do
-      it "displays move link" do
-        find('li[data-id="' + @root.id.to_s + '"] > .toolbox-cell button').click
-        wait_for_ajax_to_complete
-        expect(page).to have_css('.toolbox-items li a.ajaxbox', text: "Move", visible: true)
-      end
-    end
-
-    context "when pressing move link", js: true do
-      it "displays move dialog" do
-        find('li[data-id="' + @root.id.to_s + '"] > .toolbox-cell button').click
-        wait_for_ajax_to_complete
-        find('.toolbox-items li a.ajaxbox', text: "Move").click
-        wait_for_ajax_to_complete
-        expect(page).to have_css('.dialog.move-node-dialog', visible: true)
-      end
-    end
-
     context "when moving node to another parent", js: true do
-      it "removes selected node from current position" do
-        find('li[data-id="' + @node.id.to_s + '"] > .toolbox-cell button').click
-        wait_for_ajax_to_complete
-        find('.toolbox-items li a.ajaxbox', text: "Move").click
-        wait_for_ajax_to_complete
-        find('.dialog.move-node-dialog .action-tree ul li .node-cell button', text: "Root").click
-
-        expect(page).not_to have_css('li > .node-cell a', text: "Main", visible: true)
-      end
-
       it "moves selected node to new position" do
         find('li[data-id="' + @node.id.to_s + '"] > .toolbox-cell button').click
-        wait_for_ajax_to_complete
         find('.toolbox-items li a.ajaxbox', text: "Move").click
-        wait_for_ajax_to_complete
         find('.dialog.move-node-dialog .action-tree ul li .node-cell button', text: "Root").click
+        expect(page).not_to have_css('li > .node-cell a', text: "Main", visible: true)
+
         find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
 
         expect(page).to have_css('li > .node-cell a', text: "Main", count: 1, visible: true)
