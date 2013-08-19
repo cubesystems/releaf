cc5052880c169176b4b5c90108eeb5f1cc3cd4a2
uldis <uldis@cubesystems.lv>
2013-08-19 09:48:39 +0300

    Move, copy unsuccessfull test case and create new node test cases

--- spec/features/content_controller_spec.rb
+++ spec/features/content_controller_spec.rb
@@ -13,6 +13,37 @@ describe Releaf::ContentController do
     visit releaf_nodes_path
   end
 
+  describe "new node" do
+    context "when creating node under root", js: true do
+      it "creates new node in content tree" do
+        find(".tools .button.primary", text: "Create new item").click
+        find('.dialog.add-child-dialog .content_types ul li a', text: "ContactsController").click
+        
+        fill_in("resource_name", :with => "Main contacts")
+        find('.view-edit .new_resource .tools button.primary').click
+        
+        visit releaf_nodes_path
+
+        expect(page).to have_css('li > .node-cell a', text: "Main contacts", count: 1, visible: true)
+      end
+    end
+
+    context "when creating node under another node", js: true do
+      it "creates new node" do
+        find('li[data-id="' + @node.id.to_s + '"] > .toolbox-cell button').click
+        find('.toolbox-items li a.ajaxbox', text: "Add child").click
+        find('.dialog.add-child-dialog .content_types ul li a', text: "ContactsController").click
+
+        fill_in("resource_name", :with => "Contacts")
+        find('.view-edit .new_resource .tools button.primary').click
+
+        visit releaf_nodes_path
+        find('li[data-id="' + @node.id.to_s + '"] > .collapser-cell button').click
+        expect(page).to have_css('li > .node-cell a', text: "Contacts", count: 1, visible: true)
+      end
+    end
+  end
+
   describe "tree collapsing" do
     context "when not opened before" do
       it "do not show node children", js: true do
@@ -73,6 +104,16 @@ describe Releaf::ContentController do
         expect(page).to have_css('li > .node-cell a', text: "Main", count: 2, visible: true)
       end
     end
+
+    context "when copying node under itself", js: true do
+      it "displays flash error message" do
+        find('li[data-id="' + @node.id.to_s + '"] > .toolbox-cell button').click
+        find('.toolbox-items li a.ajaxbox', text: "Copy").click
+        find('.dialog.copy-node-dialog .action-tree ul li .node-cell button', text: "Main").click
+
+        expect(page).to have_css('.notifications .notification .message', text: "Copy not possible", visible: true)
+      end
+    end
   end
 
   describe "move node to" do
@@ -88,5 +129,16 @@ describe Releaf::ContentController do
         expect(page).to have_css('li > .node-cell a', text: "Main", count: 1, visible: true)
       end
     end
+
+    context "when moving node under itself", js: true do
+      it "displays flash error message" do
+        find('li[data-id="' + @node.id.to_s + '"] > .toolbox-cell button').click
+        find('.toolbox-items li a.ajaxbox', text: "Move").click
+        find('.dialog.move-node-dialog .action-tree ul li .node-cell button', text: "Main").click
+
+        expect(page).to have_css('.notifications .notification .message', text: "Move not possible", visible: true)
+      end
+    end
+
   end
 end
