78dd3c1339f9bbc15ebb967dbbb41e5aaf9f086c
uldis <uldis@cubesystems.lv>
2013-08-20 13:57:32 +0300

    Delete redirect tests for index and edit views

--- spec/features/edit_actions_spec.rb
+++ spec/features/edit_actions_spec.rb
@@ -6,20 +6,13 @@ feature "Base controller index actions", js: true do
     FactoryGirl.create(:book, title: "bad book")
   end
 
-  scenario "keep search parameters when navigating to edit and back" do
+  scenario "keep search params after deleting record from edit view" do
     visit admin_books_path(search: "good")
     click_link("good book")
-    click_link("Back to list")
-
-    expect(page).to have_css('.main > .table > tbody .row', :count => 1)
-  end
-
-
-  scenario "keep search parameters after delete" do
-    visit admin_books_path(search: "good")
     find('.toolbox button.trigger').click
     find('.toolbox-items li a.ajaxbox', text: "Delete").click
     find('.dialog.delete_dialog .footer button.danger', text: "Yes").click
+
     expect(page).to have_css('.main > .table th .nothing_found', :count => 1, :text => "Nothing found")
   end
 end
