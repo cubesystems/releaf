e02928341204d4615c32de5821eb8827429fb7bb
Aldis Berjoza <graudeejs@yandex.ru>
2013-08-20 15:26:18 +0300

    Use rspec 2.0 expect syntax instead of should

--- spec/helpers/javascript_helper_spec.rb
+++ spec/helpers/javascript_helper_spec.rb
@@ -11,11 +11,11 @@ describe Releaf::JavascriptHelper do
 
     context 'when argument is a string' do
       it "returns a String" do
-        helper.jquery_date_format("asd").should be_an_instance_of String
+        expect( helper.jquery_date_format("asd") ).to be_a(String)
       end
 
       it "translates Ruby date format to jQuery date format" do
-        helper.jquery_date_format("%H:%M:%S").should == 'HH:mm:ss'
+        expect( helper.jquery_date_format("%H:%M:%S") ).to eq('HH:mm:ss')
       end
     end
   end
