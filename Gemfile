c996502c50ea8904a5a0c9f4152454b3278a878d
miks <miks@cubesystems.lv>
2013-08-26 00:42:08 +0300

    Coveralls added

--- Gemfile
+++ Gemfile
@@ -47,6 +47,9 @@ group :development, :test, :demo do
   # gem 'guard-yard'
 end
 
+group :test do
+  gem 'coveralls', :require => false
+end
 
 # Declare any dependencies that are still in development here instead of in
 # your gemspec. These might include edge Rails or gems from your path or
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,23 @@
 require 'simplecov'
 require 'simplecov-rcov'
+require 'coveralls'
 SimpleCov.command_name 'rspec'
+
+Coveralls.wear!('rails')
+
+if ENV["COVERAGE"]
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::RcovFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+  SimpleCov.start do
+    add_filter '/config/'
+    add_filter '/lib/tasks'
+    add_filter '/spec/'
+    add_filter '/app/assets/'
+  end
+end
 
 
 development = !!ENV['GUARD_NOTIFY'] || !ENV["RAILS_ENV"]
@@ -12,6 +29,7 @@ require 'rspec/autorun'
 require 'factory_girl'
 require 'capybara/rspec'
 require 'support/helpers'
+
 
 Rails.backtrace_cleaner.remove_silencers!
 # Load support files
