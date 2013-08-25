e72e8818630bdac72817dcb5d9a9aacb79798ce4
miks <miks@cubesystems.lv>
2013-08-25 23:59:27 +0300

    Travis config added

--- templates/releaf/installer.rb
+++ templates/releaf/installer.rb
@@ -59,21 +59,31 @@ end
 
 GEMFILE
 
-if ENV['DUMMY_DATABASE_FILE']
-  run 'cp ' + ENV['DUMMY_DATABASE_FILE'] + ' config/database.yml'
+# collect dummy database config
+if ENV['RELEAF_DUMMY_DB_USERNAME'].nil?
+  db_username = ask_wizard("Database username? (leave blank to use the 'root')", 'root')
 else
-  @current_recipe = "database"
-  mysql_username = ask_wizard("Username for MySQL? (leave blank to use the 'root')", 'root')
-  gsub_file "config/database.yml", /username: .*/, "username: #{mysql_username}"
-
-  mysql_password = ask_wizard("Password for MySQL user #{mysql_username}?", '')
-  gsub_file "config/database.yml", /password:/, "password: #{mysql_password}"
-
-  mysql_database = ask_wizard("MySQL database name (leave blank to use 'releaf_dummy')?", 'releaf_dummy')
-  gsub_file "config/database.yml", /database: dummy_/, "database: #{mysql_database}_"
+  db_username = ENV['RELEAF_DUMMY_DB_USERNAME']
 end
-gsub_file 'config/boot.rb', "'../../Gemfile'", "'../../../../Gemfile'"
 
+if ENV['RELEAF_DUMMY_DB_PASSWORD'].nil?
+  db_password = ask_wizard("Database password for '#{db_username}?'", '')
+else
+  db_password = ENV['RELEAF_DUMMY_DB_PASSWORD']
+end
+
+if ENV['RELEAF_DUMMY_DB_NAME'].nil?
+  @current_recipe = "database"
+  db_name = ask_wizard("MySQL database name (leave blank to use 'releaf_dummy')?", 'releaf_dummy')
+else
+  db_name = ENV['RELEAF_DUMMY_DB_NAME']
+end
+
+gsub_file "config/database.yml", /username: .*/, "username: #{db_username}"
+gsub_file "config/database.yml", /database: dummy_/, "database: #{db_name}_"
+gsub_file "config/database.yml", /password:/, "password: #{db_password}" unless db_password.blank?
+
+gsub_file 'config/boot.rb', "'../../Gemfile'", "'../../../../Gemfile'"
 
 files_to_remove = %w[
   public/index.html
