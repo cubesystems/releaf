dd74c5184274080b66d648bef7de15b8b7ef97c2
miks <miks@cubesystems.lv>
2013-08-26 18:09:00 +0300

    Role feature refactored

--- spec/features/roles_spec.rb
+++ spec/features/roles_spec.rb
@@ -1,45 +1,57 @@
 require 'spec_helper'
-
-describe Releaf::RolesController do
-  before do
+feature "Roles management", js: true do
+  background do
     auth_as_admin
     @role = Releaf::Role.first
   end
 
-  it "should test roles custom fields and methods" do
-    # index should contain only one role
+  scenario "create new role" do
     visit releaf_roles_path
-    page.should have_css('.main > .table > tbody .row', :count => 1)
-
-    # save new item and redirect to show view
     click_link "Create new item"
     within("form.new_resource") do
-      page.should have_select('Default controller', :options => Releaf.available_admin_controllers.map{|controller|  I18n.t(controller, :scope => 'admin.menu_items')})
       fill_in("Name", :with => "second role")
-      select(I18n.t("admin/books", :scope => 'admin.menu_items'), :from => 'Default controller')
-      check(I18n.t("admin/books", :scope => 'admin.menu_items'))
       click_button 'Save'
     end
 
-    new_role = Releaf::Role.last
-    current_path.should eq(edit_releaf_role_path(new_role))
+    expect(page).to have_css('body > .notifications .notification[data-id="resource_status"][data-type="success"]', text: "Created")
+  end
 
-    # edit view should contain all saved variables
-    page.should have_css('.main h2.header', :text => "second role")
+  scenario "update existing role" do
+    visit releaf_roles_path
+    click_link @role.name
     within("form.edit_resource") do
-      page.should have_select('Default controller', :selected => I18n.t(new_role.default_controller, :scope => 'admin.menu_items'))
-      Releaf.available_admin_controllers.each do |controller|
-        if controller == "admin/books"
-          page.should have_checked_field(I18n.t(controller, :scope => 'admin.menu_items'))
-        else
-          page.should have_unchecked_field(I18n.t(controller, :scope => 'admin.menu_items'))
-        end
-      end
+      fill_in("Name", :with => "new name")
+      click_button 'Save'
     end
 
-    # index should contain two roles
+    expect(page).to have_css('body > .notifications .notification[data-id="resource_status"][data-type="success"]', text: "Updated")
+  end
+
+  scenario "change role default controller" do
     visit releaf_roles_path
-    page.should have_css('.main > .table > tbody .row', :count => 2)
-    page.should have_css('.main > .table > tbody .row[data-id="' + new_role.id.to_s  + '"] a:last', :text => I18n.t(new_role.default_controller, :scope => 'admin.menu_items'))
+    click_link @role.name
+    within("form.edit_resource") do
+      select('Admin/books', :from => 'Default controller')
+      click_button 'Save'
+    end
+
+    expect(page).to have_select('Default controller', selected: 'Admin/books')
+  end
+
+  scenario "change role controller permissions" do
+    visit releaf_roles_path
+    click_link @role.name
+    within("form.edit_resource") do
+      uncheck('Admin/books')
+      click_button 'Save'
+    end
+
+    Releaf.available_admin_controllers.each do |controller|
+      if controller == "admin/books"
+        expect(page).to have_unchecked_field(I18n.t(controller, :scope => 'admin.menu_items'))
+      else
+        expect(page).to have_checked_field(I18n.t(controller, :scope => 'admin.menu_items'))
+      end
+    end
   end
 end
