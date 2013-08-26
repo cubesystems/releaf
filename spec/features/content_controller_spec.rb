f05da9eb60e817792fdb11e8ce3614896b4abf2a
miks <miks@cubesystems.lv>
2013-08-26 22:06:28 +0300

    Refactor ContentController feature test

--- spec/features/content_controller_spec.rb
+++ spec/features/content_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-describe Releaf::ContentController do
+describe Releaf::ContentController, js: true do
   before do
     @admin = auth_as_admin
     # build little tree
@@ -14,51 +14,46 @@ describe Releaf::ContentController do
   end
 
   describe "new node" do
-    context "when creating node under root", js: true do
+    context "when creating node under root" do
       it "creates new node in content tree" do
-        find(".tools .button.primary", text: "Create new item").click
-        find('.dialog.add-child-dialog .content_types ul li a', text: "ContactsController").click
-        
-        fill_in("resource_name", :with => "Main contacts")
-        find('.view-edit .new_resource .tools button.primary').click
-        
-        visit releaf_nodes_path
+        click_link("Create new item")
+        click_link("ContactsController")
+        fill_in("resource_name", :with => "RootNode2")
+        click_button('Save')
 
-        expect(page).to have_css('li > .node-cell a', text: "Main contacts", count: 1, visible: true)
+        expect(page).to have_content('Releaf/content RootNode2')
       end
     end
 
-    context "when creating node under another node", js: true do
+    context "when creating node under another node" do
       it "creates new node" do
-        find('li[data-id="' + @node.id.to_s + '"] > .toolbox-cell button').click
-        find('.toolbox-items li a.ajaxbox', text: "Add child").click
-        find('.dialog.add-child-dialog .content_types ul li a', text: "ContactsController").click
+        find('li[data-id="' + @root.id.to_s + '"] > .toolbox-cell button').click
+        click_link("Add child")
+        click_link("ContactsController")
 
         fill_in("resource_name", :with => "Contacts")
-        find('.view-edit .new_resource .tools button.primary').click
+        click_button('Save')
 
-        visit releaf_nodes_path
-        find('li[data-id="' + @node.id.to_s + '"] > .collapser-cell button').click
-        expect(page).to have_css('li > .node-cell a', text: "Contacts", count: 1, visible: true)
+        expect(page).to have_content('Releaf/content RootNode Contacts')
       end
     end
   end
 
   describe "tree collapsing" do
     context "when not opened before" do
-      it "do not show node children", js: true do
+      it "do not show node children" do
         expect(page).to have_css('li[data-id="' + @root.id.to_s + '"].collapsed')
       end
     end
 
     context "when click to uncollapse node" do
-      it "show node children", js: true do
+      it "show node children" do
         find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
 
         expect(page).to have_css('li[data-id="' + @root.id.to_s + '"]:not(.collapsed)')
       end
 
-      it "keep opened node children visibility permanent", js: true do
+      it "keep opened node children visibility permanent" do
         find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
         expect { @admin.settings.last.try(:value) == true }.to become_true
         visit releaf_nodes_path
@@ -66,7 +61,7 @@ describe Releaf::ContentController do
         expect(page).to have_css('li[data-id="' + @root.id.to_s + '"]:not(.collapsed)')
       end
 
-      it "keep closed node children visibility permanent", js: true do
+      it "keep closed node children visibility permanent" do
         find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
         find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
         expect{ @admin.settings.last.try(:value) == false }.to become_true
@@ -82,63 +77,61 @@ describe Releaf::ContentController do
       visit edit_releaf_node_path @node
     end
 
-    context "when going to node from toolbox list", js: true do
+    context "when going to node from toolbox list" do
       it "navigates to targeted node's edit view" do
         find('.toolbox button').click
-        find('.toolbox-items li a.ajaxbox', text: "Go to").click
-        find('.dialog.goto-node-dialog .action-tree ul li .node-cell a', text: "RootNode").click
+        click_link("Go to")
+        click_link("RootNode")
 
-        expect(page).to have_css('.view-edit .edit_resource h2.header', text: 'RootNode', visible: true)
+        expect(page).to have_css('.view-edit .edit_resource h2.header', text: 'RootNode')
       end
     end
   end
 
   describe "copy node to" do
-    context "when copying node", js: true do
+    context "when copying node" do
       it "shows copied node in tree" do
         find('li[data-id="' + @node.id.to_s + '"] > .toolbox-cell button').click
-        find('.toolbox-items li a.ajaxbox', text: "Copy").click
-        find('.dialog.copy-node-dialog .action-tree ul li .node-cell button', text: "RootNode").click
-        find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
+        click_link("Copy")
+        click_button("RootNode")
+        expect(page).to have_css('.notifications .notification .message', text: "Copied")
 
-        expect(page).to have_css('li > .node-cell a', text: "Main", count: 2, visible: true)
+        find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
+        expect(page).to have_css('li > .node-cell a', text: "Main", count: 2)
       end
     end
 
-    context "when copying node under itself", js: true do
+    context "when copying node under itself" do
       it "displays flash error message" do
         find('li[data-id="' + @node.id.to_s + '"] > .toolbox-cell button').click
-        find('.toolbox-items li a.ajaxbox', text: "Copy").click
-        find('.dialog.copy-node-dialog .action-tree ul li .node-cell button', text: "Main").click
-
-        expect(page).to have_css('.notifications .notification .message', text: "Copy not possible", visible: true)
+        click_link("Copy")
+        click_button("Main")
+        expect(page).to have_css('.notifications .notification .message', text: "Copy not possible")
       end
     end
   end
 
   describe "move node to" do
-    context "when moving node to another parent", js: true do
+    context "when moving node to another parent" do
       it "moves selected node to new position" do
         find('li[data-id="' + @node.id.to_s + '"] > .toolbox-cell button').click
-        find('.toolbox-items li a.ajaxbox', text: "Move").click
-        find('.dialog.move-node-dialog .action-tree ul li .node-cell button', text: "RootNode").click
-        expect(page).not_to have_css('li > .node-cell a', text: "Main", visible: true)
+        click_link("Move")
+        click_button("RootNode")
 
+        expect(page).to have_css('.notifications .notification .message', text: "Moved")
         find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
-
-        expect(page).to have_css('li > .node-cell a', text: "Main", count: 1, visible: true)
+        expect(page).to have_css('li > .node-cell a', text: "Main", count: 1)
       end
     end
 
-    context "when moving node under itself", js: true do
+    context "when moving node under itself" do
       it "displays flash error message" do
         find('li[data-id="' + @node.id.to_s + '"] > .toolbox-cell button').click
-        find('.toolbox-items li a.ajaxbox', text: "Move").click
-        find('.dialog.move-node-dialog .action-tree ul li .node-cell button', text: "Main").click
+        click_link("Move")
+        click_button("Main")
 
-        expect(page).to have_css('.notifications .notification .message', text: "Move not possible", visible: true)
+        expect(page).to have_css('.notifications .notification .message', text: "Move not possible")
       end
     end
-
   end
 end
