1f5ebffa0f24ffc03a7550cc65b29ec03d32a9a8
miks <miks@cubesystems.lv>
2013-08-26 13:32:00 +0300

    Do not pollute test logs with cache requests

--- lib/generators/releaf/templates/initializers/dragonfly.rb
+++ lib/generators/releaf/templates/initializers/dragonfly.rb
@@ -1,1 +1,9 @@
 require 'dragonfly/rails/images'
+if Rails.env.test?
+  Rails.application.middleware.delete Rack::Cache
+  Rails.application.middleware.insert 0, Rack::Cache, {
+    :verbose     => false,
+    :metastore   => URI.encode("file:#{Rails.root}/tmp/dragonfly/cache/meta"), # URI encoded in case of spaces
+    :entitystore => URI.encode("file:#{Rails.root}/tmp/dragonfly/cache/body")
+  }
+end
