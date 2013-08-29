8c723ff860dd9bd0686de6c4540f773e2feb6234
miks <miks@cubesystems.lv>
2013-08-29 13:18:59 +0300

    Dummy texts controller and view added

--- lib/generators/dummy/templates/config/routes.rb
+++ lib/generators/dummy/templates/config/routes.rb
@@ -3,5 +3,9 @@ Dummy::Application.routes.draw do
     releaf_resources :books, :authors
   end
 
+  Releaf::Node::Route.for(Text).each do|route|
+    get route.params('texts#show')
+  end
+
   root :to => 'home#index'
 end
