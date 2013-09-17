f315f241158435d83fb2847fcd763402f8477ec0
Aldis Berjoza <graudeejs@yandex.ru>
2013-09-17 17:36:28 +0300

    Fix typo

--- app/assets/javascripts/releaf/lib/tinymce/attachment_upload/langs/ru_dlg.js
+++ app/assets/javascripts/releaf/lib/tinymce/attachment_upload/langs/ru_dlg.js
@@ -1,7 +1,7 @@
 tinyMCE.addI18n('ru.attachment_upload_dlg', {
-  title: 'Вставить фаил',
-  header: "Вставить фаил",
-  input:  "Выберите фаил",
+  title: 'Вставить файл',
+  header: "Вставить файл",
+  input:  "Выберите файл",
   insert: "Вставить",
   cancel: "Отмена"
 });
