4b576f757618aeef2ba181c67b85e62d3d023f07
Aldis Berjoza <graudeejs@yandex.ru>
2013-09-17 21:42:01 +0300

    Add translations

--- app/assets/javascripts/releaf/lib/tinymce/attachment_upload/langs/en_dlg.js
+++ app/assets/javascripts/releaf/lib/tinymce/attachment_upload/langs/en_dlg.js
@@ -1,5 +1,6 @@
 tinyMCE.addI18n('en.attachment_upload_dlg', {
   title: 'Insert file',
+  file_title: 'Title',
   header: "Insert file",
   input:  "Choose a file",
   insert: "Insert",
--- app/assets/javascripts/releaf/lib/tinymce/attachment_upload/langs/lv_dlg.js
+++ app/assets/javascripts/releaf/lib/tinymce/attachment_upload/langs/lv_dlg.js
@@ -1,5 +1,6 @@
 tinyMCE.addI18n('lv.attachment_upload_dlg', {
     title:  'Augšupielādēt failu',
+    file_title: 'Название',
     header: "Augšupielādēt failu",
     input:  "Izvēlieties failu",
     insert: "Augšupielādēt",
