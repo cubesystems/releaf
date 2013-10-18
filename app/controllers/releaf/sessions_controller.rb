5caad5ad02eb815573c1c705f40ff9fd0f19bde3
Aldis Berjoza <graudeejs@yandex.ru>
2013-10-18 13:56:23 +0300

    Fix issue #89

--- app/controllers/releaf/sessions_controller.rb
+++ app/controllers/releaf/sessions_controller.rb
@@ -2,4 +2,11 @@ module Releaf
   class SessionsController < Devise::SessionsController
     layout "releaf/admin"
   end
+
+  protected
+
+  def after_sign_in_path_for resource
+    releaf_root_path
+  end
+
 end
