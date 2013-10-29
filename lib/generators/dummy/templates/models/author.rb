ecd8b67159a0ffed23cf26b372a1cf5404b88e2d
miks <miks@cubesystems.lv>
2013-10-29 17:15:27 +0200

    Dummy::Author: fix depreciation warning
    by using restrict_with_exception

--- lib/generators/dummy/templates/models/author.rb
+++ lib/generators/dummy/templates/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ActiveRecord::Base
   validates_presence_of :name
 
-  has_many :books, dependent: :restrict
+  has_many :books, dependent: :restrict_with_exception
 
   def to_text
     return "#{name} #{surname}"
