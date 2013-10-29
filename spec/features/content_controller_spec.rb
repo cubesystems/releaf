cbb2250cdc34c24353deca3100974d264fcf5e54
miks <miks@cubesystems.lv>
2013-10-29 18:06:26 +0200

    Fix test by using sleep instead of db check

--- spec/features/content_controller_spec.rb
+++ spec/features/content_controller_spec.rb
@@ -59,7 +59,7 @@ describe Releaf::ContentController, js: true do
 
       it "keeps opened node children visibility permanent" do
         find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
-        expect { @admin.settings.last.try(:value) == true }.to become_true
+        sleep 3
         visit releaf_nodes_path
 
         expect(page).to have_css('li[data-id="' + @root.id.to_s + '"]:not(.collapsed)')
@@ -68,7 +68,7 @@ describe Releaf::ContentController, js: true do
       it "keeps closed node children visibility permanent" do
         find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
         find('li[data-id="' + @root.id.to_s + '"] > .collapser-cell button').click
-        expect{ @admin.settings.last.try(:value) == false }.to become_true
+        sleep 3
         visit releaf_nodes_path
 
         expect(page).to have_css('li[data-id="' + @root.id.to_s + '"].collapsed')
