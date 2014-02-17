93fa9e89b573ce1df1489b30a6f8d62182581460
miks <miks@cubesystems.lv>
2014-02-17 09:16:25 +0200

    Keep current url after authorization

--- app/controllers/releaf/sessions_controller.rb
+++ app/controllers/releaf/sessions_controller.rb
@@ -11,7 +11,12 @@ module Releaf
     protected
 
     def after_sign_in_path_for resource
-      releaf_root_path
+      sign_in_url = url_for(action: 'new', only_path: true)
+      if URI(request.referer).path == sign_in_url
+        super
+      else
+        stored_location_for(resource) || releaf_root_path
+      end
     end
   end
 end
