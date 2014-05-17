5257a978b87db49aa56a7d4c59b89acb685b460a
miks <miks@cubesystems.lv>
2014-05-17 23:38:56 +0300

    Reorganize common javascripts

---
+++ releaf-core/app/assets/javascripts/releaf/include/profile_settings.js
@@ -0,0 +1,28 @@
+jQuery(function(){
+
+    var body = jQuery('body');
+
+    var settings_url = jQuery('header .profile').data('settings-url');
+    body.on('settingssave', function( event, key_or_settings, value )
+    {
+        if (!settings_url)
+        {
+            return;
+        }
+
+        var settings = key_or_settings;
+        if (typeof settings == "string")
+        {
+            settings = {};
+            settings[key_or_settings] = value;
+        }
+
+        jQuery.ajax
+        ({
+            url:  settings_url,
+            data: { "settings": settings},
+            type: 'POST',
+            dataType: 'json'
+        });
+    });
+});
