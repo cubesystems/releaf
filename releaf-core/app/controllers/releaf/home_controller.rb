1cc10652720e380e17485b16c43cda0d6850e26c
Aldis Berjoza <graudeejs@yandex.ru>
2014-10-11 14:16:56 +0300

    Fix issue #136

--- releaf-core/app/controllers/releaf/home_controller.rb
+++ releaf-core/app/controllers/releaf/home_controller.rb
@@ -1,16 +1,33 @@
 module Releaf
   class HomeController < BaseController
     def index
-      user = self.send("current_#{ReleafDeviseHelper.devise_admin_model_name}")
-      unless user.nil?
+      @user = self.send("current_#{ReleafDeviseHelper.devise_admin_model_name}")
+      unless @user.nil?
         respond_to do |format|
-          format.html { redirect_to url_for(action: 'index', controller: '/' + user.role.default_controller) }
+          format.html { redirect_to default_or_available_controller_path }
         end
       end
     end
 
     def page_not_found
       error_response('page_not_found', 404)
+    end
+
+    protected
+
+    def default_or_available_controller_path
+      controllers_to_try.each do |controller_string|
+        begin
+          return url_for(action: 'index', controller: "/#{controller_string}")
+        rescue ActionController::UrlGenerationError
+          next
+        end
+      end
+      return root_path
+    end
+
+    def controllers_to_try
+      [@user.role.default_controller, @user.role.permissions].flatten.uniq
     end
   end
 end
