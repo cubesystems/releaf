c5a0803f689fd4af7ad6332d5b846de5a3fb70ad
miks <miks@cubesystems.lv>
2014-10-13 23:51:53 +0300

    ButtonHelper: icon only" mode implemented

--- releaf-core/app/helpers/releaf/button_helper.rb
+++ releaf-core/app/helpers/releaf/button_helper.rb
@@ -2,7 +2,7 @@ module Releaf
   module ButtonHelper
     def releaf_button(text, icon, attributes = {})
       default_attributes = {
-        class: "button",
+        class: ["button"],
         title: text
       }
 
@@ -11,6 +11,14 @@ module Releaf
       else
         default_attributes[:type] = "button"
         tag = :button
+      end
+
+      if text.blank?
+        default_attributes[:class] << "only-icon"
+        # title is required for only-icon buttons / links
+        raise ArgumentError, "Title missing for icon-only button/link" if attributes[:title].blank?
+      else
+        default_attributes[:class] << "with-icon"
       end
 
       content_tag(tag, merge_attributes(default_attributes, attributes)) do
