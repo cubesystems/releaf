bd38ae491659b38674ac1783a7bfca26d1ce778f
miks <miks@cubesystems.lv>
2014-11-28 00:18:08 +0200

    Releaf::Content::NodeFormBuilder: tests added

--- releaf-content/app/helpers/releaf/content/node_form_builder.rb
+++ releaf-content/app/helpers/releaf/content/node_form_builder.rb
@@ -1,15 +1,19 @@
 module Releaf::Content
   class NodeFormBuilder < Releaf::FormBuilder
     def render_locale
-      options = {
-         select_options: I18n.available_locales,
-         include_blank: object.locale.blank?
+      releaf_item_field(:locale, options: render_locale_options)
+    end
+
+    def render_locale_options
+      {
+        select_options: I18n.available_locales,
+        include_blank: object.locale.blank?
       }
-      releaf_item_field(:locale, options: options)
     end
 
     def render_content_type
-      releaf_text_field(:content_type, input: {disabled: true, value: I18n.t(object.content_type.underscore, scope: 'admin.content_types')})
+      content_type_input_options = {disabled: true, value: I18n.t(object.content_type.underscore, scope: 'admin.content_types')}
+      releaf_text_field(:content_type, input: content_type_input_options)
     end
 
     def render_slug
@@ -28,14 +32,17 @@ module Releaf::Content
     end
 
     def render_item_position
-      options = {
-        include_blank: false,
-        select_options: template.options_for_select(item_position_options, object.item_position)
-      }
-      releaf_item_field(:item_position, options: options)
+      releaf_item_field(:item_position, options: item_position_options)
     end
 
     def item_position_options
+      {
+        include_blank: false,
+        select_options: template.options_for_select(item_position_select_options, object.item_position)
+      }
+    end
+
+    def item_position_select_options
       after_text = I18n.t('After', scope: 'admin.global')
       list = [[I18n.t('First', scope: 'admin.global'), 0]]
       template.controller.instance_variable_get(:@order_nodes).each do |node|
@@ -45,13 +52,14 @@ module Releaf::Content
       list
     end
 
-    private
+    def slug_base_url
+      "#{template.request.protocol}#{template.request.host_with_port}#{object.parent.try(:url)}/"
+    end
 
     def slug_link
-      base_url = "#{template.request.protocol}#{template.request.host_with_port}#{object.parent.try(:url)}/"
       template.link_to(object.url) do
         safe_join do
-          [base_url, tag(:span, object.slug), '/']
+          [slug_base_url, tag(:span, object.slug), '/']
         end
       end
     end
--- releaf-content/spec/helpers/node_form_builder_spec.rb
+++ releaf-content/spec/helpers/node_form_builder_spec.rb
@@ -1,28 +1,97 @@
 require "spec_helper"
 
-describe Releaf::Content::NodeFormBuilder, type: :class, pending: true do
-  class FormBuilderTestHelper < ActionView::Base; end
+describe Releaf::Content::NodeFormBuilder, type: :class do
+  class FormBuilderTestHelper < ActionView::Base
+    include Releaf::ApplicationHelper
+    include Releaf::ButtonHelper
+    include FontAwesome::Rails::IconHelper
+    def controller_scope_name; end
+    def generate_url_releaf_content_nodes_path(args); end
+  end
+
   let(:template){ FormBuilderTestHelper.new }
-  let(:object){ Node.new }
+  let(:object){ Node.new(content_type: "Text", slug: "b", id: 2,
+                         parent: Node.new(content_type: "Text", slug: "a", id: 1)) }
   let(:subject){ described_class.new(:resource, object, template, {}) }
 
   describe "#render_locale" do
-    it "renders customized field" do
+    it "renders locale with #render_locale_options" do
+      allow(subject).to receive(:render_locale_options).and_return(a: "b")
+      allow(subject).to receive(:releaf_item_field).with(:locale, options: {a: "b"}).and_return("x")
+      expect(subject.render_locale).to eq("x")
+    end
+  end
+
+  describe "#render_locale_options" do
+    it "returns :select_options and :include_blank values" do
+      expect(subject.render_locale_options.keys).to eq([:select_options, :include_blank])
+    end
+
+    it ":select_options contains all available locales" do
+      allow(I18n).to receive(:available_locales).and_return([:lt, :et])
+      expect(subject.render_locale_options[:select_options]).to eq([:lt, :et])
+    end
+
+    context "when subject have defined locale" do
+      it ":include_blank is false" do
+        subject.object.locale = :lt
+        expect(subject.render_locale_options[:include_blank]).to be false
+      end
+    end
+
+    context "when subject have no locale" do
+      it ":include_blank is true" do
+        subject.object.locale = nil
+        expect(subject.render_locale_options[:include_blank]).to be true
+      end
     end
   end
 
   describe "#render_content_type" do
-    it "renders customized field" do
+    it "renders disabled content type field with localized content type value" do
+      options = {disabled: true, value: "Translated content type"}
+      allow(I18n).to receive(:t).with("text", scope: "admin.content_types").and_return("Translated content type")
+      allow(subject).to receive(:releaf_text_field).with(:content_type, input: options).and_return("x")
+      expect(subject.render_content_type).to eq("x")
     end
   end
 
   describe "#render_slug" do
     it "renders customized field" do
+      allow(subject).to receive(:slug_base_url).and_return("http://localhost/parent")
+      allow(template).to receive(:generate_url_releaf_content_nodes_path).with(parent_id: 1, id: 2).and_return("http://localhost/slug-generation-url")
+
+      content = '<div class="field type-text" data-name="slug"><div class="label-wrap"><label for="resource_slug">Slug</label></div><div class="value"><input data-generator-url="http://localhost/slug-generation-url" id="resource_slug" name="resource[slug]" type="text" value="b" /><button class="button only-icon secondary generate" title="Suggest slug" type="button"><i class="fa fa-keyboard-o"></i></button></div><div class="link"><a href="/a/b">http://localhost/parent<span>b</span>/</a></div></div>'
+
+      expect(subject.render_slug).to eq(content)
     end
   end
 
   describe "#render_item_position" do
-    it "renders customized field" do
+    it "renders locale with #item_position_options" do
+      allow(subject).to receive(:item_position_options).and_return(a: "b")
+      allow(subject).to receive(:releaf_item_field).with(:item_position, options: {a: "b"}).and_return("x")
+      expect(subject.render_item_position).to eq("x")
+    end
+  end
+
+  describe "#item_position_options" do
+    before do
+      object.item_position = 2
+      allow(subject).to receive(:item_position_select_options).and_return([["a", 1], ["b", 2], ["c", 3]])
+    end
+
+    it "returns :select_options and :include_blank values" do
+      expect(subject.item_position_options.keys).to eq([:include_blank, :select_options])
+    end
+
+    it ":select_options correct select options" do
+      options = '<option value="1">a</option>'
+      options << "\n"
+      options << '<option selected="selected" value="2">b</option>'
+      options << "\n"
+      options << '<option value="3">c</option>'
+      expect(subject.item_position_options[:select_options]).to eq(options)
     end
   end
 end
