633a3b1672a7a809b250af23baa94d7b1e361d2b
miks <miks@cubesystems.lv>
2014-12-06 22:36:21 +0200

    Features matchers: breadcrumbs matcher implemented

--- lib/releaf/rspec/features_matchers.rb
+++ lib/releaf/rspec/features_matchers.rb
@@ -13,6 +13,12 @@ module Capybara
       has_css?("#{primary_header_css_rule} .totals", text: "#{count} Resources found")
     end
 
+    def has_breadcrumbs?(*items)
+      items.each_with_index do|item, index|
+        has_css?("main header nav ul.breadcrumbs li:nth-child(#{index}) a", text: item)
+      end
+    end
+
     def has_notification?(text, type="success")
       result = has_css?(".notifications .notification[data-type='#{type}']", text: text)
       if first(".notifications button.close")
