4c9ecdfcf961369f324e12ae32f1ccc68411a15e
miks <miks@cubesystems.lv>
2015-01-14 16:53:44 +0200

    Builders: dynamic default builder scope

--- releaf-core/app/helpers/releaf/builders.rb
+++ releaf-core/app/helpers/releaf/builders.rb
@@ -1,23 +1,27 @@
 class Releaf::Builders
   def self.builder_class(controller_class, builder_type)
-    builder_namespace = controller_class.name.gsub(/Controller$/, "")
+    controller_namespace = controller_class.name.gsub(/Controller$/, "")
     builer_type_name = "#{builder_type.to_s.camelize}Builder"
-    builder_class_name = "#{builder_namespace}::#{builer_type_name}"
+    builder_class_name = "#{controller_namespace}::#{builer_type_name}"
 
     begin
       Object.const_get(builder_class_name).is_a?(Class)
     rescue NameError => e
       allowed_errors = [
-        "uninitialized constant #{builder_namespace}",
+        "uninitialized constant #{controller_namespace}",
         "uninitialized constant #{builder_class_name}"
       ]
       if allowed_errors.include?(e.message)
-        builder_class_name = "Releaf::Builders::#{builder_type.to_s.camelize}Builder"
+        builder_class_name = [default_namespace, builer_type_name].join("::")
       else
         raise
       end
     end
 
     builder_class_name.constantize
+  end
+
+  def self.default_namespace
+    name
   end
 end
--- releaf-core/spec/helpers/builders_spec.rb
+++ releaf-core/spec/helpers/builders_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 
 describe Releaf::Builders, type: :class do
   module Admin::Advanced
+    class SickBuilder
+    end
     class AuthorsController
     end
   end
+
+  class Admin::Advanced::Builders < Releaf::Builders; end
+
   describe ".builder_class" do
     it "returns builder class for given controller and builder type" do
       expect(described_class.builder_class(Releaf::Permissions::UsersController, :form)).to eq(Releaf::Permissions::Users::FormBuilder)
@@ -13,7 +18,8 @@ describe Releaf::Builders, type: :class do
     context "when custom builder class does not exists for given controller and builder type" do
       it "returns default builder class" do
         expect(described_class.builder_class(Admin::Advanced::AuthorsController, :form)).to eq(Releaf::Builders::FormBuilder)
-        expect(described_class.builder_class(Releaf::Permissions::UsersController, :index)).to eq(Releaf::Builders::IndexBuilder)
+        allow(described_class).to receive(:default_namespace).and_return("Admin::Advanced")
+        expect(described_class.builder_class(Releaf::Permissions::UsersController, :sick)).to eq(Admin::Advanced::SickBuilder)
       end
     end
 
@@ -26,6 +32,13 @@ describe Releaf::Builders, type: :class do
         allow(Object).to receive(:const_get).with("Admin::Authors::FormBuilder").and_raise(NameError, "uninitialized constant asdasd")
         expect{ described_class.builder_class(Admin::AuthorsController, :form) }.to raise_error(NameError, "uninitialized constant asdasd")
       end
+    end
+  end
+
+  describe ".default_namespace" do
+    it "returns own class name" do
+      expect(described_class.default_namespace).to eq("Releaf::Builders")
+      expect(Admin::Advanced::Builders.default_namespace).to eq("Admin::Advanced::Builders")
     end
   end
 end
