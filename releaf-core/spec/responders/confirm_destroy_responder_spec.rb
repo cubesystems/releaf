97bf914fc17750ca1066c6b6fe384ae8d50e3c22
miks <miks@cubesystems.lv>
2015-02-15 16:17:36 +0200

    Tests for all responders added

---
+++ releaf-core/spec/responders/confirm_destroy_responder_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+describe Releaf::Responders::ConfirmDestroyResponder, type: :controller do
+  let(:controller){ Releaf::BaseController.new }
+  let(:resource){ Book.new}
+  subject{ described_class.new(controller, [resource]) }
+
+  describe "#to_html" do
+    context "when options has key :destroyable with `true` value" do
+      it "renders default view" do
+        allow(subject).to receive(:options).and_return(destroyable: true)
+        expect(subject).to receive(:default_render)
+        subject.to_html
+      end
+    end
+
+    context "when options has key :destroyable with `false` value" do
+      it "renders `refused_destroy` template" do
+        allow(subject).to receive(:options).and_return(destroyable: false)
+        expect(subject).to receive(:render).with("refused_destroy")
+        subject.to_html
+      end
+    end
+  end
+end
+
