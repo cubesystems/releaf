7417abffb838e2d33a77301bfd70f551c0c3036d
miks <miks@cubesystems.lv>
2015-04-25 10:00:32 +0300

    Errors matcher implemented

--- lib/releaf/rspec/features_matchers.rb
+++ lib/releaf/rspec/features_matchers.rb
@@ -20,6 +20,37 @@ module Capybara
       has_css?("#{primary_header_css_rule} .totals", text: "#{count} Resources found")
     end
 
+
+    # Allows to match againg validation errors within forms
+    # Support either model specific (base) errors with:
+    #     expect(page).to have_error('Global form error message')
+    # and attribute specific errors with:
+    #     expect(page).to have_error('Taken', field: 'Lol')
+    #
+    # @param error_message [String] error message to find
+    # @param options [Hash] available option is `field` that can be anything that is normally accepted by fill_in
+    #     e.g., the label text or the id of the textarea
+    # @return [true] whether errors has been found otherwise will raise Capybara::ElementNotFound exception
+    def has_error?(error_message, options = {})
+      error_exists = false
+      if options[:field]
+        all(".field.has-error").each do|field|
+          if find(:field, options[:field]) && first(".error", text: error_message)
+            error_exists = true
+            break
+          end
+        end
+      else
+        if first(".form-error-box .error", text: error_message)
+          error_exists = true
+        end
+      end
+
+      raise Capybara::ElementNotFound.new("Unable to find given error message") unless error_exists
+
+      true
+    end
+
     def has_breadcrumbs?(*items)
       items.each_with_index do|item, index|
         has_css?("main header nav ul.breadcrumbs li:nth-child(#{index}) a", text: item)
