0986f2327387eea369bfef6d4edfac901159a38e
miks <miks@cubesystems.lv>
2015-04-25 09:44:43 +0300

    Update releaf available controllers tests with mising publisher controller

--- releaf-core/spec/lib/releaf_spec.rb
+++ releaf-core/spec/lib/releaf_spec.rb
@@ -4,6 +4,7 @@ describe Releaf do
   describe ".available_controllers" do
     it "returns all available controllers" do
       expect(Releaf.available_controllers).to eq(["releaf/content/nodes", "admin/books", "admin/authors",
+                                                  "admin/publishers",
                                                   "releaf/permissions/users", "releaf/permissions/roles",
                                                   "releaf/core/settings", "releaf/i18n_database/translations",
                                                   "admin/chapters", "releaf/permissions/profile"])
