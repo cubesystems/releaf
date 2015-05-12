f4cf63b148c1b1756013b25273001cd37fd1381b
miks <miks@cubesystems.lv>
2015-05-13 00:17:19 +0300

    Reloadable sidebar

--- releaf-core/app/assets/javascripts/releaf/include/sidebar.js
+++ releaf-core/app/assets/javascripts/releaf/include/sidebar.js
@@ -8,115 +8,143 @@ jQuery(function(){
         body.trigger('sidecompactcloseall');
     });
 
-    var first_level_side_items =  jQuery('body > aside > nav > ul > li');
-
-    jQuery('body > aside > .compacter button').click(function()
+    body.bind('sidebarinit', function(e)
     {
-        var button = jQuery(this);
-        var icon = button.find('i').first();
+        var target = jQuery(e.target);
 
-        if (body.hasClass('side-compact'))
-        {
-            body.trigger('sidecompactcloseall');
-            body.trigger( 'settingssave', [ "releaf.side.compact", false ] );
-            body.removeClass('side-compact');
-            icon.addClass('fa-angle-double-left').removeClass('fa-angle-double-right');
-        }
-        else
-        {
-            body.trigger( 'settingssave', [ "releaf.side.compact", true ] );
-            body.addClass('side-compact');
-            icon.addClass('fa-angle-double-right').removeClass('fa-angle-double-left');
-        }
-        body.trigger('sidecompactchange');
-    });
-
-    body.bind('sidecompactchange', function()
-    {
-        if (body.hasClass('side-compact'))
-        {
-            first_level_side_items.each(function()
-            {
-                var trigger = jQuery(this).children('.trigger');
-                trigger.attr( 'title', trigger.children('.name').text() );
-            });
-        }
-        else
-        {
-            first_level_side_items.children('.trigger').removeAttr('title');
-        }
-    });
-
-    body.trigger('sidecompactchange');
-
-    jQuery('body > aside > nav .collapser button').click(function(e)
-    {
-        var item = jQuery(this).closest('li');
         e.stopPropagation();
 
-        item.toggleClass('collapsed');
-        jQuery(this).blur();
+        var sidebar;
+        if (target.is('aside'))
+        {
+            sidebar = target;
+        }
+        else
+        {
+            sidebar = target.find('aside');
+        }
 
-        var collapser_icon = item.find('.collapser i');
-        var collapsed = item.hasClass('collapsed');
-
-        collapser_icon.toggleClass('fa-chevron-down', collapsed);
-        collapser_icon.toggleClass('fa-chevron-up',  !collapsed);
-
-        var setting_key = 'releaf.menu.collapsed.' + item.data('name');
-        body.trigger( 'settingssave', [ setting_key, collapsed ]  );
-
-    });
-
-    first_level_side_items.bind('sidecompactitemopen', function()
-    {
-        body.trigger('sidecompactcloseall');
-        jQuery(this).addClass('open');
-        side_compact_overlay.show();
-    });
-
-    first_level_side_items.bind('sidecompactitemclose', function()
-    {
-        jQuery(this).removeClass('open');
-        side_compact_overlay.hide();
-    });
-
-
-    first_level_side_items.bind('sidecompacttoggle', function()
-    {
-        var item   = jQuery(this);
-        var event = (item.is('.open')) ? 'sidecompactitemclose' : 'sidecompactitemopen';
-        item.trigger( event );
-    });
-
-    body.bind('sidecompactcloseall', function()
-    {
-        first_level_side_items.filter('.open').trigger('sidecompactitemclose');
-    });
-
-    jQuery('body > header').click(function()
-    {
-        // add additional trigger on header to close opened compact submenu
-        // because header is above the side compact overlay
-        if (!body.hasClass('side-compact') || first_level_side_items.filter('.open').length < 1)
+        if (sidebar.length < 1)
         {
             return;
         }
 
-        body.trigger('sidecompactcloseall');
-        return false;
+        var first_level_side_items =  sidebar.find('nav > ul > li');
+
+        sidebar.find('.compacter button').click(function()
+        {
+            var button = jQuery(this);
+            var icon = button.find('i').first();
+
+            if (body.hasClass('side-compact'))
+            {
+                body.trigger('sidecompactcloseall');
+                body.trigger( 'settingssave', [ "releaf.side.compact", false ] );
+                body.removeClass('side-compact');
+                icon.addClass('fa-angle-double-left').removeClass('fa-angle-double-right');
+            }
+            else
+            {
+                body.trigger( 'settingssave', [ "releaf.side.compact", true ] );
+                body.addClass('side-compact');
+                icon.addClass('fa-angle-double-right').removeClass('fa-angle-double-left');
+            }
+            body.trigger('sidecompactchange');
+        });
+
+        body.unbind('sidecompactchange').bind('sidecompactchange', function()
+        {
+            if (body.hasClass('side-compact'))
+            {
+                first_level_side_items.each(function()
+                {
+                    var trigger = jQuery(this).children('.trigger');
+                    trigger.attr( 'title', trigger.children('.name').text() );
+                });
+            }
+            else
+            {
+                first_level_side_items.children('.trigger').removeAttr('title');
+            }
+        });
+
+        body.trigger('sidecompactchange');
+
+        sidebar.find('> nav .collapser button').click(function(e)
+        {
+            var item = jQuery(this).closest('li');
+            e.stopPropagation();
+
+            item.toggleClass('collapsed');
+            jQuery(this).blur();
+
+            var collapser_icon = item.find('.collapser i');
+            var collapsed = item.hasClass('collapsed');
+
+            collapser_icon.toggleClass('fa-chevron-down', collapsed);
+            collapser_icon.toggleClass('fa-chevron-up',  !collapsed);
+
+            var setting_key = 'releaf.menu.collapsed.' + item.data('name');
+            body.trigger( 'settingssave', [ setting_key, collapsed ]  );
+
+        });
+
+        first_level_side_items.bind('sidecompactitemopen', function()
+        {
+            body.trigger('sidecompactcloseall');
+            jQuery(this).addClass('open');
+            side_compact_overlay.show();
+        });
+
+        first_level_side_items.bind('sidecompactitemclose', function()
+        {
+            jQuery(this).removeClass('open');
+            side_compact_overlay.hide();
+        });
+
+
+        first_level_side_items.bind('sidecompacttoggle', function()
+        {
+            var item   = jQuery(this);
+            var event = (item.is('.open')) ? 'sidecompactitemclose' : 'sidecompactitemopen';
+            item.trigger( event );
+        });
+
+        body.unbind('sidecompactcloseall').bind('sidecompactcloseall', function()
+        {
+            first_level_side_items.filter('.open').trigger('sidecompactitemclose');
+        });
+
+        jQuery('body > header').click(function()
+        {
+            // add additional trigger on header to close opened compact submenu
+            // because header is above the side compact overlay
+            if (!body.hasClass('side-compact') || first_level_side_items.filter('.open').length < 1)
+            {
+                return;
+            }
+
+            body.trigger('sidecompactcloseall');
+            return false;
+        });
+
+        sidebar.find('> nav span.trigger').click(function()
+        {
+            if (body.hasClass('side-compact'))
+            {
+                var item  = jQuery(this).closest('li');
+                item.trigger('sidecompacttoggle');
+            }
+            else
+            {
+                jQuery(this).find('.collapser button').trigger('click');
+            }
+        });
     });
 
-    jQuery('body > aside > nav span.trigger').click(function()
+    // attach toolboxinit to all loaded content
+    body.on('contentloaded', function(e)
     {
-        if (body.hasClass('side-compact'))
-        {
-            var item  = jQuery(this).closest('li');
-            item.trigger('sidecompacttoggle');
-        }
-        else
-        {
-            jQuery(this).find('.collapser button').trigger('click');
-        }
+        jQuery(e.target).trigger('sidebarinit');
     });
 });
