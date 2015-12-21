008e8754a828ba11d0f77530e7ef1ebda63915b2
gnc <guntis@cubesystems.lv>
2015-12-21 11:36:57 +0200

    Improve DRYness of match_html matcher

--- lib/releaf/rspec/matchers.rb
+++ lib/releaf/rspec/matchers.rb
@@ -1,12 +1,16 @@
 RSpec::Matchers.define :match_html do |expected|
 
-
   match do |actual|
-    actual.strip.gsub(/\s+/,' ').gsub('> <', '><') == expected.strip.gsub(/\s+/,' ').gsub('> <', '><')
+    normalize_html(actual) == normalize_html(expected)
+  end
+
+  def normalize_html string
+    string.strip.gsub(/\s+/,' ').gsub('> <', '><')
   end
 
   failure_message do |actual|
     "expected that #{actual} would match the HTML structure of #{expected}"
   end
+
 
 end
