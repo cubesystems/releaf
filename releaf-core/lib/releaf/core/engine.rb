c3d5c2e75ce901d778a91cf9ce2568356117c617
miks <miks@cubesystems.lv>
2016-01-26 20:48:08 +0200

    Correctly include Releaf route mapper

--- releaf-core/lib/releaf/core/engine.rb
+++ releaf-core/lib/releaf/core/engine.rb
@@ -23,13 +23,15 @@ module Releaf::Core
   end
 
   class Engine < ::Rails::Engine
-    initializer 'precompile', group: :all do |app|
+    initializer 'releaf.assets_precompile', group: :all do |app|
       app.config.assets.precompile += %w(ckeditor/*)
       app.config.assets.precompile += %w(releaf/application.css releaf/controllers/*.css releaf/*.js releaf/*.png releaf/*.gif releaf/*.ico)
     end
+
+    initializer 'releaf.route_mapper',
+      after: 'action_dispatch.prepare_dispatcher' do |app|
+      ActionDispatch::Routing::Mapper.send(:include, Releaf::Core::RouteMapper)
+    end
   end
 
-  ActiveSupport.on_load :action_controller do
-    ActionDispatch::Routing::Mapper.send(:include, Releaf::Core::RouteMapper)
-  end
 end
