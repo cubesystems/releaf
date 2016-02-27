e827187446743424ee172db6c96255794005cc38
miks <miks@cubesystems.lv>
2016-02-27 09:49:07 +0200

    Add TranslationsStore class for translation data management

--- releaf-i18n_database/spec/lib/releaf/i18n_database/backend_spec.rb
+++ releaf-i18n_database/spec/lib/releaf/i18n_database/backend_spec.rb
@@ -1,12 +1,7 @@
 require "rails_helper"
 
 describe Releaf::I18nDatabase::Backend do
-
-  before do
-    allow( Releaf.application.config.i18n_database ).to receive(:create_missing_translations).and_return(true)
-    allow( I18n.backend ).to receive(:reload_cache?).and_return(true)
-    I18n.backend.reload_cache
-  end
+  let(:translations_store){ Releaf::I18nDatabase::TranslationsStore.new }
 
   describe ".configure_component" do
     it "adds new `Releaf::I18nDatabase::Configuration` configuration with enabled missing translation creation" do
@@ -25,18 +20,41 @@ describe Releaf::I18nDatabase::Backend do
     end
   end
 
+  describe "#translations" do
+    let(:another_translations_store){ Releaf::I18nDatabase::TranslationsStore.new }
+
+    context "when translations has been loaded and is not expired" do
+      it "returns assigned translations hash instance" do
+        subject.translations_cache = translations_store
+        allow(translations_store).to receive(:expired?).and_return(false)
+        expect(Releaf::I18nDatabase::TranslationsStore).to_not receive(:new)
+        expect(subject.translations).to eq(translations_store)
+      end
+    end
+
+    context "when translations has been loaded and is expired" do
+      it "initializes new `TranslationsStore`, cache and return it" do
+        subject.translations_cache = translations_store
+        allow(translations_store).to receive(:expired?).and_return(true)
+        expect(Releaf::I18nDatabase::TranslationsStore).to receive(:new).and_return(another_translations_store)
+        expect(subject.translations).to eq(another_translations_store)
+      end
+    end
+
+    context "when translations has not been loaded" do
+      it "initializes new `TranslationsStore`, cache and return it" do
+        subject.translations_cache = nil
+        expect(Releaf::I18nDatabase::TranslationsStore).to receive(:new).and_return(another_translations_store)
+        expect(subject.translations).to eq(another_translations_store)
+      end
+    end
+  end
+
   describe "#store_translations" do
     it "merges given translations to cache" do
-      translation = FactoryGirl.create(:translation, key: "admin.content.save")
-      FactoryGirl.create(:translation_data, translation: translation, localization: "save", lang: "en")
-      FactoryGirl.create(:translation_data, translation: translation, localization: "saglabāt", lang: "lv")
-      I18n.backend.reload_cache
-      allow( I18n.backend ).to receive(:reload_cache?).and_return(false)
-
-      expect{ I18n.backend.store_translations(:en, {admin: {profile: "profils"}}) }.to change{ I18n.t("admin.profile") }.
-        from("Profile").to("profils")
-
-      expect(I18n.t("admin.content.save", locale: "lv")).to eq("saglabāt")
+      allow(subject).to receive(:translations).and_return(translations_store)
+      expect(translations_store).to receive(:add).with(:lv, a: "x")
+      subject.store_translations(:lv, a: "x")
     end
   end
 
@@ -64,33 +82,6 @@ describe Releaf::I18nDatabase::Backend do
     end
   end
 
-  describe "#create_missing_translation?" do
-    before do
-      allow( Releaf.application.config.i18n_database ).to receive(:create_missing_translations).and_return(true)
-    end
-
-    context "when missing translation creation is enabled globally by i18n config and not disabled by `create_missing` option" do
-      it "returns true" do
-        expect(subject.send(:create_missing_translation?, {})).to be true
-        expect(subject.send(:create_missing_translation?, create_missing: true)).to be true
-        expect(subject.send(:create_missing_translation?, create_missing: nil)).to be true
-      end
-    end
-
-    context "when missing translation creation is disabled globally by i18n config" do
-      it "returns false" do
-        allow( Releaf.application.config.i18n_database ).to receive(:create_missing_translations).and_return(false)
-        expect(subject.send(:create_missing_translation?, {})).to be false
-      end
-    end
-
-    context "when missing translation creation is disabled by `create_missing` option" do
-      it "returns false" do
-        expect(subject.send(:create_missing_translation?, create_missing: false)).to be false
-      end
-    end
-  end
-
   describe ".translations_updated_at" do
     it "returns translations updated_at from cached settings" do
       allow(Releaf::Settings).to receive(:[]).with(described_class::UPDATED_AT_KEY).and_return("x")
@@ -105,258 +96,63 @@ describe Releaf::I18nDatabase::Backend do
     end
   end
 
-  describe "#reload_cache?" do
-    context "when last translation update differs from last cache load" do
-      it "returns true" do
-        allow(described_class).to receive(:translations_updated_at).and_return(1)
-        described_class::CACHE[:updated_at] = 2
-        expect(subject.reload_cache?).to be true
-      end
-    end
-
-    context "when last translation update does not differ from last cache load but translations has not been loaded" do
-      it "returns true" do
-        allow(described_class).to receive(:translations_updated_at).and_return(1)
-        described_class::CACHE[:updated_at] = 1
-        described_class::CACHE[:translations] = nil
-        expect(subject.reload_cache?).to be true
-      end
-    end
-
-    context "when last translation update does not differ from last cache load" do
-      it "returns false" do
-        allow(described_class).to receive(:translations_updated_at).and_return(1)
-        described_class::CACHE[:updated_at] = 1
-        expect(subject.reload_cache?).to be false
-      end
-    end
-  end
-
-  describe "#reload_cache" do
-    it "resets missing array" do
-      I18n.t("something")
-      expect{ I18n.backend.reload_cache }.to change{ described_class::CACHE[:missing].blank? }.from(false).to(true)
-    end
-
-    it "writes last translations update timestamp to cache" do
-      allow(described_class).to receive(:translations_updated_at).and_return("x")
-      expect{ I18n.backend.reload_cache }.to change{ described_class::CACHE[:updated_at] }.to("x")
-    end
-
-    it "loads all translated data to cache as hash" do
-      translation = FactoryGirl.create(:translation, key: "admin.xx.save")
-      FactoryGirl.create(:translation_data, translation: translation, localization: "saglabāt", lang: "lv")
-      FactoryGirl.create(:translation_data, translation: translation, localization: "save", lang: "en")
-
-      expect{ I18n.backend.reload_cache }.to change{ described_class::CACHE[:translations].blank? }.from(true).to(false)
-
-      expect(described_class::CACHE[:translations][:lv][:admin][:xx][:save]).to eq("saglabāt")
-      expect(described_class::CACHE[:translations][:en][:admin][:xx][:save]).to eq("save")
-    end
-  end
-
   describe "#lookup" do
-    describe "cache reload" do
-      let(:timestamp){ Time.now }
+    before do
+      allow(subject).to receive(:translations).and_return(translations_store)
+      allow(subject).to receive(:normalize_flat_keys).with(:lv, "some.localization", "_scope_", ":")
+        .and_return("xx.s.loc")
+    end
 
-      context "when cache timestamp differs from translations update timestamp" do
-        it "reloads cache" do
-          described_class::CACHE[:updated_at] = timestamp
-          allow(described_class).to receive(:translations_updated_at).and_return(timestamp + 1.day)
-          expect(I18n.backend).to receive(:reload_cache)
-          I18n.t("cancel")
-        end
-      end
+    it "flattens key before passing further" do
+      expect(translations_store).to receive(:missing?).with(:lv, "xx.s.loc")
+      expect(translations_store).to receive(:lookup).with(:lv, "xx.s.loc", separator: ":", a: "b")
+      expect(translations_store).to receive(:missing).with(:lv, "xx.s.loc", separator: ":", a: "b")
 
-      context "when cache timestamp is same as translations update timestamp" do
-        it "does not reload cache" do
-          allow( I18n.backend ).to receive(:reload_cache?).and_call_original
-          described_class::CACHE[:updated_at] = timestamp
-          allow(described_class).to receive(:translations_updated_at).and_return(timestamp)
+      subject.lookup(:lv, "some.localization", "_scope_", separator: ":", a: "b")
+    end
 
-          expect(I18n.backend).to_not receive(:reload_cache)
-          I18n.t("cancel")
-        end
+    context "when translation is known as missing" do
+      it "does not make lookup in translation hash, does not mark it as missing and return nil" do
+        allow(translations_store).to receive(:missing?).with(:lv, "xx.s.loc").and_return(true)
+        expect(translations_store).to_not receive(:lookup)
+        expect(translations_store).to_not receive(:missing)
+        expect(subject.lookup(:lv, "some.localization", "_scope_", separator: ":", a: "b")).to be nil
       end
     end
 
-    context "when translation exists within higher level key (instead of being scope)" do
-      it "returns nil (Humanize key)" do
-        translation = FactoryGirl.create(:translation, key: "some.food")
-        FactoryGirl.create(:translation_data, translation: translation, lang: "lv", localization: "suņi")
-        expect(I18n.t("some.food", locale: "lv")).to eq("suņi")
-        expect(I18n.t("some.food.asd", locale: "lv")).to eq("Asd")
-      end
-    end
-
-    context "when pluralized translation requested" do
-      context "when valid pluralized data matched" do
-        it "returns pluralized translation" do
-          translation = FactoryGirl.create(:translation, key: "dog.other")
-          FactoryGirl.create(:translation_data, translation: translation, lang: "lv", localization: "suņi")
-          expect(I18n.t("dog", locale: "lv", count: 2)).to eq("suņi")
-        end
+    context "when translation is not known as missing" do
+      before do
+        allow(translations_store).to receive(:missing?).with(:lv, "xx.s.loc").and_return(false)
       end
 
-      context "when invalid pluralized data matched" do
-        it "returns nil (Humanize key)" do
-          translation = FactoryGirl.create(:translation, key: "dog.food")
-          FactoryGirl.create(:translation_data, translation: translation, lang: "lv", localization: "suņi")
-          expect(I18n.t("dog", locale: "lv", count: 2)).to eq("Dog")
-        end
-      end
-    end
-
-    context "existing translation" do
-      context "when translation exists with different case" do
-        it "returns existing translation" do
-          translation = FactoryGirl.create(:translation, key: "Save")
-          FactoryGirl.create(:translation_data, translation: translation, lang: "lv", localization: "Saglabāt")
-          I18n.backend.reload_cache
-
-          expect(I18n.t("save", locale: "lv")).to eq("Saglabāt")
-          expect(I18n.t("Save", locale: "lv")).to eq("Saglabāt")
-        end
-      end
-
-      context "when translations hash exists in parent scope" do
+      context "when lookup result is not nil" do
         before do
-          translation = FactoryGirl.create(:translation, key: "dog.other")
-          FactoryGirl.create(:translation_data, translation: translation, lang: "en", localization: "dogs")
+          allow(translations_store).to receive(:lookup).with(:lv, "xx.s.loc", separator: ":", a: "b").and_return("x")
         end
 
-        context "when pluralized translation requested" do
-          it "returns pluralized translation" do
-            expect(I18n.t("admin.controller.dog", count: 2)).to eq("dogs")
-          end
+        it "returns lookup result" do
+          expect(subject.lookup(:lv, "some.localization", "_scope_", separator: ":", a: "b")).to eq("x")
         end
 
-        context "when non pluralized translation requested" do
-          it "returns nil" do
-            expect(I18n.t("admin.controller.dog")).to eq("Dog")
-          end
+        it "does not mark translation as missing" do
+          expect(translations_store).to_not receive(:missing).with(:lv, "xx.s.loc", separator: ":", a: "b")
+          subject.lookup(:lv, "some.localization", "_scope_", separator: ":", a: "b")
         end
       end
 
-      context "when translation has default" do
-        context "when default creation is disabled" do
-          it "creates base translation" do
-            expect{ I18n.t("xxx.test.mest", default: :"xxx.mest", create_default: false) }.to change{ Releaf::I18nDatabase::Translation.pluck(:key) }
-              .to(["xxx.test.mest"])
-
-          end
-        end
-
-        context "when default creation is not disabled" do
-          it "creates base and default translations" do
-            expect{ I18n.t("xxx.test.mest", default: :"xxx.mest") }.to change{ Releaf::I18nDatabase::Translation.pluck(:key) }
-              .to(match_array(["xxx.mest", "xxx.test.mest"]))
-          end
-        end
-      end
-
-      context "in parent scope" do
-        context "nonexistent translation in given scope" do
-          it "uses parent scope" do
-            translation = FactoryGirl.create(:translation, key: "validation.admin.blank")
-            FactoryGirl.create(:translation_data, translation: translation, lang: "lv", localization: "Tukšs")
-            expect(I18n.t("blank", scope: "validation.admin.roles", locale: "lv")).to eq("Tukšs")
-          end
-
-          context "when `inherit_scopes` option is `false`" do
-            it "does not lookup upon higher level scopes" do
-              translation = FactoryGirl.create(:translation, key: "validation.admin.blank")
-              FactoryGirl.create(:translation_data, translation: translation, lang: "lv", localization: "Tukšs")
-              expect(I18n.t("blank", scope: "validation.admin.roles", locale: "lv", inherit_scopes: false)).to eq("Blank")
-            end
-          end
-        end
-
-        context "and empty translation value in given scope" do
-          it "uses parent scope" do
-            parent_translation = FactoryGirl.create(:translation, key: "validation.admin.blank")
-            FactoryGirl.create(:translation_data, translation: parent_translation, lang: "lv", localization: "Tukšs")
-
-            translation = FactoryGirl.create(:translation, key: "validation.admin.roles.blank")
-            FactoryGirl.create(:translation_data, translation: translation, lang: "lv", localization: "")
-
-            expect(I18n.t("blank", scope: "validation.admin.roles", locale: "lv")).to eq("Tukšs")
-          end
-        end
-
-        context "and existing translation value in given scope" do
-          it "uses given scope" do
-            parent_translation = FactoryGirl.create(:translation, key: "validation.admin.blank")
-            FactoryGirl.create(:translation_data, translation: parent_translation, lang: "lv", localization: "Tukšs")
-
-            translation = FactoryGirl.create(:translation, key: "validation.admin.roles.blank")
-            FactoryGirl.create(:translation_data, translation: translation, lang: "lv", localization: "Tukša vērtība")
-
-            expect(I18n.t("blank", scope: "validation.admin.roles", locale: "lv")).to eq("Tukša vērtība")
-          end
-        end
-      end
-
-      context "when scope defined" do
-        it "uses given scope" do
-          translation = FactoryGirl.create(:translation, key: "admin.content.cancel")
-          FactoryGirl.create(:translation_data, translation: translation, lang: "lv", localization: "Atlikt")
-          expect(I18n.t("cancel", scope: "admin.content", locale: "lv")).to eq("Atlikt")
-        end
-      end
-    end
-
-    context "nonexistent translation" do
-      context "loading multiple times" do
-        it "queries db only for the first time" do
-          I18n.t("save", scope: "admin.xx")
-          expect(Releaf::I18nDatabase::Translation).not_to receive(:where)
-          I18n.t("save", scope: "admin.xx")
-        end
-      end
-
-      context "with nonexistent translation" do
+      context "when lookup result is nil" do
         before do
-          allow(Releaf.application.config).to receive(:all_locales).and_return(["ru", "lv"])
+          allow(translations_store).to receive(:lookup).with(:lv, "xx.s.loc", separator: ":", a: "b").and_return(nil)
         end
 
-        it "creates empty translation" do
-          expect { I18n.t("save") }.to change { Releaf::I18nDatabase::Translation.where(key: "save").count }.by(1)
+        it "returns nil" do
+          expect(subject.lookup(:lv, "some.localization", "_scope_", separator: ":", a: "b")).to be nil
         end
 
-        context "when count option passed" do
-          context "when create_plurals option not passed" do
-            it "creates empty translation" do
-              expect { I18n.t("animals.horse", count: 1) }.to change { Releaf::I18nDatabase::Translation.where(key: "animals.horse").count }.by(1)
-            end
-          end
-
-          context "when negative create_plurals option passed" do
-            it "creates empty translation" do
-              expect { I18n.t("animals.horse", create_plurals: false, count: 1) }.to change { Releaf::I18nDatabase::Translation.where(key: "animals.horse").count }.by(1)
-            end
-          end
-
-          context "when positive create_plurals option passed" do
-            it "creates pluralized translations for all Releaf locales" do
-              result = ["animals.horse.few", "animals.horse.many", "animals.horse.one", "animals.horse.other", "animals.horse.zero"]
-              expect{ I18n.t("animals.horse", count: 1, create_plurals: true) }.to change{ Releaf::I18nDatabase::Translation.pluck(:key).sort }.
-                from([]).to(result.sort)
-            end
-          end
+        it "marks translation as missing" do
+          expect(translations_store).to receive(:missing).with(:lv, "xx.s.loc", separator: ":", a: "b")
+          subject.lookup(:lv, "some.localization", "_scope_", separator: ":", a: "b")
         end
-      end
-    end
-
-    context "when scope requested" do
-      it "returns all scope translations" do
-        translation1 = FactoryGirl.create(:translation, key: "admin.content.cancel")
-        FactoryGirl.create(:translation_data, translation: translation1, lang: "lv", localization: "Atlikt")
-        translation2 = FactoryGirl.create(:translation, key: "admin.content.save")
-        FactoryGirl.create(:translation_data, translation: translation2, lang: "lv", localization: "Saglabāt")
-
-        expect(I18n.t("admin.content", locale: "lv")).to eq({cancel: "Atlikt", save: "Saglabāt"})
-        expect(I18n.t("admin.content", locale: "en")).to eq({cancel: nil, save: nil})
       end
     end
   end
