cf9ad803d3b213a52f914ef15a02be6c20b1f751
miks <miks@cubesystems.lv>
2016-03-04 13:03:51 +0200

    Do not attempt to humanize when translation key is nil

--- releaf-i18n_database/spec/lib/releaf/i18n_database/humanize_missing_translations_spec.rb
+++ releaf-i18n_database/spec/lib/releaf/i18n_database/humanize_missing_translations_spec.rb
@@ -2,9 +2,15 @@ require "rails_helper"
 
 describe Releaf::I18nDatabase::HumanizeMissingTranslations do
   describe ".call" do
-    context "when exception is I18n::MissingTranslation" do
+    context "when key is present and exception is I18n::MissingTranslation" do
       it "humanizes missing translations" do
         expect(I18n.t("some.missing translation")).to eq("Missing translation")
+      end
+    end
+
+    context "when key is not present and exception is I18n::MissingTranslation" do
+      it "does not intercept it" do
+        expect(I18n.t(nil)).to eq("translation missing: en.no key")
       end
     end
 
