3fa7461933d20604f843dbfeec1cda1e57328599
miks <miks@cubesystems.lv>
2016-05-12 21:53:34 +0300

    Refactorize default controller resolver with tests and subdomain support

--- releaf-core/lib/releaf/root/default_controller_resolver.rb
+++ releaf-core/lib/releaf/root/default_controller_resolver.rb
@@ -5,33 +5,29 @@ module Releaf::Root
 
     def call
       controllers.each do |controller_name|
-        return controller_index_path(controller_name) if controller_index_exists?(controller_name)
+        path = controller_index_path(controller_name)
+        return path if path
       end
+
+      nil
     end
 
     def controller_index_path(controller_name)
-      Rails.application.routes.url_helpers.url_for(action: "index", controller: controller_name, only_path: true)
+      route_options = {controller: controller_name, action: "index"}
+
+      subdomain.present? && route_path(route_options.merge(subdomain: subdomain)) || route_path(route_options)
     end
 
-    def controller_index_exists?(controller_name)
-      route_options = { controller: controller_name, action: "index" }
-      if subdomain.present?
-        # If subdomain is present try to find route matching it
-        # since it'll be most specific route
-        subdomain_route_options = route_options.merge(subdomain: subdomain)
-        return true if route_defaults.include?(subdomain_route_options)
-      end
-      route_defaults.include?(route_options)
+    def route_path(route_options)
+      Rails.application.routes.url_helpers.url_for(route_options.merge(only_path: true)) if route_exists?(route_options)
     end
 
     def controllers
       Releaf.application.config.available_controllers
     end
 
-    private
-
-    def route_defaults
-      @route_defaults ||= Rails.application.routes.routes.map(&:defaults)
+    def route_exists?(route_options)
+      Rails.application.routes.routes.map(&:defaults).include?(route_options)
     end
 
     def subdomain
