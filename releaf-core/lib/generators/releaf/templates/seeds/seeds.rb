ab5ec40613bc66e1c16680c0d4d5fca62892b60b
miks <miks@cubesystems.lv>
2016-07-11 15:51:09 +0300

    Simplify seeds by removing extra group and user

--- releaf-core/lib/generators/releaf/templates/seeds/seeds.rb
+++ releaf-core/lib/generators/releaf/templates/seeds/seeds.rb
@@ -9,46 +9,27 @@ end
 # Role {{{
 
 puts "Creating roles"
-super_admin = Releaf::Permissions::Role.new(name: "super admin", default_controller: "releaf/permissions/users")
+role = Releaf::Permissions::Role.new(name: "super admin", default_controller: "releaf/permissions/users")
 Releaf.application.config.available_controllers.each do|controller|
-  super_admin.permissions.build(permission: "controller.#{controller}")
+  role.permissions.build(permission: "controller.#{controller}")
 end
-super_admin.save!
 
-content_manager = Releaf::Permissions::Role.new(name: "content manager", default_controller: "releaf/content/nodes")
-content_manager.permissions.build(permission: "controller.releaf/content/nodes")
-content_manager.save!
+role.save!
 
 # }}}
 # User {{{
 
 puts "Creating users"
-users = {
-  user: {
-    name: 'Admin',
-    surname: 'User',
-    password: 'password',
-    password_confirmation: 'password',
-    locale: "en",
-    email: 'admin@example.com',
-    role: super_admin,
-  },
-  simple_user: {
-    name: 'Simple',
-    surname: 'User',
-    password: 'password',
-    password_confirmation: 'password',
-    locale: "en",
-    email: 'user@example.com',
-    role: content_manager,
-  }
-}
-
-users.each_value do |attributes|
-  Releaf::Permissions::User.create!(attributes)
-end
+Releaf::Permissions::User.create!(
+  name: 'Admin',
+  surname: 'User',
+  password: 'password',
+  password_confirmation: 'password',
+  locale: "en",
+  email: 'admin@example.com',
+  role: role,
+)
 
 # }}}
 
 # vim: set fdm=marker:
-
