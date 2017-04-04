3c609cad433a62673b65b0349e387772295f7494
miks <miks@cubesystems.lv>
2017-04-04 11:24:28 +0300

    Correctly return empty features array for Releaf::RootController

--- releaf-core/spec/controllers/releaf/root_controller_spec.rb
+++ releaf-core/spec/controllers/releaf/root_controller_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe Releaf::RootController do
   login_as_user :user
 
+  describe "#features" do
+    it "has no features" do
+      expect(subject.features).to eq([])
+    end
+  end
+
   describe "GET home" do
     it "redirects to default controller resolver returned path authorized as user" do
       allow(Releaf.application.config.root.default_controller_resolver).to receive(:call)
@@ -11,7 +17,6 @@ describe Releaf::RootController do
       expect(response).to redirect_to("_randompath_")
     end
   end
-
 
   describe "PUT settings" do
     context 'when params[:settings] is not Hash' do
