7a8d1ff01606845a1fb937b13096df393bad0e77
Staņislavs Koikovs <stanislavs@Arturss-Mac-mini.local>
2017-11-16 12:01:32 +0200

    Preload node tree

--- releaf-content/app/builders/releaf/content/nodes/index_builder.rb
+++ releaf-content/app/builders/releaf/content/nodes/index_builder.rb
@@ -9,7 +9,7 @@ module Releaf::Content::Nodes
 
     def tree_resource_toolbox(resource)
       tag(:div, class: "only-icon toolbox-cell") do
-        toolbox(resource, index_path: index_path)
+        toolbox(resource[:node], index_path: index_path)
       end
     end
 
