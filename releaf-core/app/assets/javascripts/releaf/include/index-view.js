a2ad4857d91cc918693211f2572a4a061eb9e98d
Staņislavs Koikovs <stanislavs@cubesystems.lv>
2018-11-16 16:25:33 +0200

    Add Relief::Builders::PaginationBuilder

--- releaf-core/app/assets/javascripts/releaf/include/index-view.js
+++ releaf-core/app/assets/javascripts/releaf/include/index-view.js
@@ -175,7 +175,7 @@ jQuery(function()
 
     body.on('contentloaded', function(e)
     {
-        jQuery(e.target).find('#page_select').on('change', function()
+        jQuery(e.target).find('.pagination select[name="page"]').on('change', function()
         {
             var val = jQuery(this).val();
             if (val)
